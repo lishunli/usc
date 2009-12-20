132e2492450059fa67240df40c32e5943f6b64b3
nhjsjmz@gmail.com <nhjsjmz@gmail.com@35b65ada-3664-11de-8fef-1f5ae417a2df>
2009-12-20 11:58:36 +0000

    版本2.8 加入图片 在classpath中添加资源 反射的初步概念 对于classloader, 每一个.class实际就是一个Class对象 Class是对类信息的表述, 是类的metainfo / metadata 2.8_1_加入爆炸图片

--- TankWar/src/org/usc/tankwar/client/Explode.java
+++ TankWar/src/org/usc/tankwar/client/Explode.java
@@ -2,6 +2,8 @@ package org.usc.tankwar.client;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
 
 /**
  * 爆炸类
@@ -14,8 +16,33 @@ public class Explode
 	int x, y;
 	private boolean live = true;
 
-	int[] diameter =
-	{ 4, 7, 12, 18, 26, 32, 49, 30, 14, 6 };
+	private static Toolkit tk = Toolkit.getDefaultToolkit();
+
+	private static Image[] imgs =
+	{
+			tk.getImage(Explode.class.getClassLoader().getResource(
+					"images/0.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource(
+					"images/1.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource(
+					"images/2.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource(
+					"images/3.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource(
+					"images/4.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource(
+					"images/5.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource(
+					"images/6.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource(
+					"images/7.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource(
+					"images/8.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource(
+					"images/9.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource(
+					"images/10.gif")) };
+
 	int step = 0;
 
 	private TankClient tc;
@@ -27,26 +54,39 @@ public class Explode
 		this.tc = tc;
 	}
 
+	private static boolean init = false;
+	
 	public void draw(Graphics g)
 	{
+		if(!init)
+		{
+			for (int i = 0; i < imgs.length; i++)
+			{
+				g.drawImage(imgs[i], -100, -100, null);
+			}
+			init = true;
+		}
 		if (!live)
 		{
 			tc.explodes.remove(this);
 			return;
 		}
 
-		if (step == diameter.length)
+		if (step == imgs.length)
 		{
 			live = false;
 			step = 0;
 			return;
 		}
 
-		Color c = g.getColor();// 获得当前颜色
-		g.setColor(Color.ORANGE);
-		g.fillOval(x, y, diameter[step], diameter[step]);
+		// Color c = g.getColor();// 获得当前颜色
+		// g.setColor(Color.ORANGE);
+		// g.fillOval(x, y, diameter[step], diameter[step]);
+		// step++;
+
+		g.drawImage(imgs[step], x, y, null);
 		step++;
 
-		g.setColor(c);// 恢复颜色
+		// g.setColor(c);// 恢复颜色
 	}
 }
