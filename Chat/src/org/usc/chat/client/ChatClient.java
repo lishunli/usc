8d31a1d1cd7bd0751b5d711a02189f64d7bee0bf
nhjsjmz@gmail.com <nhjsjmz@gmail.com@35b65ada-3664-11de-8fef-1f5ae417a2df>
2009-12-21 14:28:33 +0000

    Chat V0.4

--- Chat/src/org/usc/chat/client/ChatClient.java
+++ Chat/src/org/usc/chat/client/ChatClient.java
@@ -4,9 +4,10 @@ import java.awt.BorderLayout;
 import java.awt.Frame;
 import java.awt.TextArea;
 import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 
 /**
  * 聊天的客户端
@@ -16,6 +17,8 @@ import java.awt.event.WindowListener;
  */
 public class ChatClient extends Frame
 {
+	private static final long serialVersionUID = 1L;
+	
 	TextField tfTxt = new TextField();
 	TextArea taContent = new TextArea();
 	
@@ -39,7 +42,20 @@ public class ChatClient extends Frame
 				System.exit(0);
 			}
 		});
+		tfTxt.addActionListener(new TFListener());
 		this.setVisible(true);
+	}
+	
+	private class TFListener implements ActionListener
+	{
+		@Override
+		public void actionPerformed(ActionEvent e)
+		{
+			String s = tfTxt.getText().trim().toString();
+			
+			taContent.setText(s);
+			tfTxt.setText("");
+		}
 	}
 	
 	
