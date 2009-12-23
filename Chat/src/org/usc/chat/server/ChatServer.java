4a40d9cf23685fe7034735bc5d24d96c438e15d1
nhjsjmz@gmail.com <nhjsjmz@gmail.com@35b65ada-3664-11de-8fef-1f5ae417a2df>
2009-12-23 11:53:41 +0000

    Chat V1.0

--- Chat/src/org/usc/chat/server/ChatServer.java
+++ Chat/src/org/usc/chat/server/ChatServer.java
@@ -17,71 +17,120 @@ import java.net.SocketException;
 public class ChatServer
 {
 	public static final int TCP_PORT = 8888;
+	boolean started = false;
+	ServerSocket ss = null;
 
 	public static void main(String[] args)
 	{
-		boolean started = false;
+		new ChatServer().start();
+	}
 
-		ServerSocket ss = null;
-		Socket s = null;
-		DataInputStream dis = null;
+	public void start()
+	{
 		try
 		{
 			ss = new ServerSocket(TCP_PORT);
-		}
-		catch (SocketException e)
+			started = true;
+		} catch (SocketException e)
 		{
 			System.out.println("端口正在使用...");
 			System.out.println("请关掉服务器程序，并重启服务器！");
 			System.exit(0);
-		}
-		catch (IOException e)
+		} catch (IOException e)
 		{
 			e.printStackTrace();
 		}
 		try
 		{
-			started = true;
 			while (started)
 			{
-				boolean bConnected = false;
-				s = ss.accept();
-				bConnected = true;
+				Socket s = ss.accept();
+				Client c = new Client(s);
+
+				new Thread(c).start();
+
 				System.out.println("A Client Connected!");
 
-				dis = new DataInputStream(s.getInputStream());
-				while (bConnected)
-				{
-					String str = dis.readUTF();
-					System.out.println(str);
-				}
 			}
 
-		} catch (EOFException e)
-		{
-			System.out.println("Client Closed!");
 		} catch (IOException e)
 		{
 			e.printStackTrace();
 		} finally
 		{
+			if (ss != null)
+			{
+				try
+				{
+					ss.close();
+				} catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+				ss = null;
+			}
+		}
+	}
+
+	class Client implements Runnable
+	{
+		private Socket s;
+		private DataInputStream dis = null;
+		private boolean bConnected = false;
+
+		public Client(Socket s)
+		{
+			this.s = s;
 			try
 			{
-				if (dis != null)
-				{
-					dis.close();
-					dis = null;
-				}
-				if (s != null)
-				{
-					s.close();
-					s = null;
-				}
-			} catch (IOException e1)
+				dis = new DataInputStream(s.getInputStream());
+				bConnected = true;
+			} catch (IOException e)
 			{
-				e1.printStackTrace();
+				e.printStackTrace();
 			}
 		}
 
+		@Override
+		public void run()
+		{
+			try
+			{
+				while (bConnected)
+				{
+					String str = dis.readUTF();
+					System.out.println(str);
+
+				}
+			} catch (EOFException e)
+			{
+				System.out.println("Client Closed!");
+			}
+
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			} finally
+			{
+				try
+				{
+					if (dis != null)
+					{
+						dis.close();
+						dis = null;
+					}
+					if (s != null)
+					{
+						s.close();
+						s = null;
+					}
+				} catch (IOException e1)
+				{
+					e1.printStackTrace();
+				}
+			}
+
+		}
 	}
+
 }
