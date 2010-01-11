5698bbfd25fec8a60daced227c781a778f271f54
nhjsjmz@gmail.com <nhjsjmz@gmail.com@35b65ada-3664-11de-8fef-1f5ae417a2df>
2010-01-11 17:03:31 +0000

    JavadocDemo

---
+++ JavadocDemo1/src/org/usc/javadoc/JavadocTest1.java
@@ -0,0 +1,14 @@
+package org.usc.javadoc;
+
+/**
+ * 测试Javadoc自定义标记
+ * 
+ * @author <a href="http://www.blogjava.net/lishunli/" target="_blank">ShunLi4</a>
+ * @Time 2010-1-11
+ * @version     %I%, %G%
+
+ */
+public class JavadocTest1
+{
+
+}
