c22d0d1622da45907d001ee1e03955461b029c0f
nhjsjmz@gmail.com <nhjsjmz@gmail.com@35b65ada-3664-11de-8fef-1f5ae417a2df>
2010-01-15 07:51:20 +0000

    日期的修改，把中文修改为英文

---
+++ JavadocDemo1/src/org/usc/javadoc/JavadocTest3.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package org.usc.javadoc;
+
+/**
+ * 日期的修改，把中文修改为英文
+ * 
+ * @author <a href="http://www.blogjava.net/lishunli/" target="_blank">ShunLi</a>
+ * @notes Created on Jan 15, 2010<br>
+ *        Revision of last commit:$Revision$<br>
+ *        Author of last commit:$Author$<br>
+ *        Date of last commit:$Date$<br>
+ *        <p>
+ */
+public class JavadocTest3
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
