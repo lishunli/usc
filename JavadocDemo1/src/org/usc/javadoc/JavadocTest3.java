33302181f7e5f99faac29c650a9061299b3ce870
nhjsjmz@gmail.com <nhjsjmz@gmail.com@35b65ada-3664-11de-8fef-1f5ae417a2df>
2010-01-15 09:05:09 +0000

    测试SVN提交的时候的Time的字符集问题

--- JavadocDemo1/src/org/usc/javadoc/JavadocTest3.java
+++ JavadocDemo1/src/org/usc/javadoc/JavadocTest3.java
@@ -1,11 +1,15 @@
 package org.usc.javadoc;
 
 /**
- *   
+ * 测试SVN提交的时候的Time的字符集问题
+ * <p>
+ * 第一必须设置为UTF-8格式
+ * <p>
+ * 第二可以设置eclipse.ini中-Duser.language=en 更改为英文显示
+ * <p>
  * 
- *
  * @author <a href="http://www.blogjava.net/lishunli/" target="_blank">ShunLi</a>
- * @notes Created on Jan 15, 2010 4:22:10 PM<br>
+ * @notes Created on 2010-1-15 下午05:01:14<br>
  *        Revision of last commit:$Revision$<br>
  *        Author of last commit:$Author$<br>
  *        Date of last commit:$Date$<br>
