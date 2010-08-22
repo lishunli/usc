459979e1795d44470fd2ecda89342ed1a69745b1
nhjsjmz@gmail.com <nhjsjmz@gmail.com@35b65ada-3664-11de-8fef-1f5ae417a2df>
2010-08-22 12:13:15 +0000

    DEV: SSHBase for eclipse, you can quickly set up project use struts2 + hibernate3 + spring 3 fw in eclipse.

---
+++ sshbase-app/sshbase-app-main/src/main/java/org/usc/service/student/IStudentService.java
@@ -0,0 +1,28 @@
+package org.usc.service.student;
+
+import java.util.List;
+
+import org.usc.bean.Student;
+import org.usc.dao.base.IBaseDao;
+
+/**
+ * 学生服务类接口
+ * 
+ * @author <a href="http://www.blogjava.net/lishunli/" target="_blank">ShunLi</a>
+ * @notes Created on 2010-1-21<br>
+ *        Revision of last commit:$Revision$<br>
+ *        Author of last commit:$Author$<br>
+ *        Date of last commit:$Date$<br>
+ *        <p>
+ */
+public interface IStudentService extends IBaseDao<Student>
+{
+	/**
+	 * 根据姓名查找学生
+	 * 
+	 * @param value
+	 *            姓名
+	 * @return 该姓名的学生集
+	 */
+	public abstract List<Student> findByName(String value);
+}
