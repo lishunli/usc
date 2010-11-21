282f3505d036a35f43cb8f91789349e77e40a63b
nhjsjmz@gmail.com <nhjsjmz@gmail.com@35b65ada-3664-11de-8fef-1f5ae417a2df>
2010-11-21 14:25:06 +0000

    小工具名加上“批量”

--- FileOperater/src/org/usc/file/operater/FileOperater.java
+++ FileOperater/src/org/usc/file/operater/FileOperater.java
@@ -44,12 +44,12 @@ public class FileOperater extends javax.swing.JFrame {
 		jLabel6 = new javax.swing.JLabel();
 
 		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-		setTitle("\u6587\u4ef6\u540d\u5927\u5c0f\u5199\u8f6c\u6362-\u987a\u5229(QQ:506817493)");
+		setTitle("\u6587\u4ef6\u540d\u5927\u5c0f\u5199\u6279\u91cf\u8f6c\u6362-\u987a\u5229(QQ:506817493)");
 		setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
 		setResizable(false);
 
 		jLabel1.setFont(new java.awt.Font("微软雅黑", 0, 24));
-		jLabel1.setText(" \u6587\u4ef6\u540d\u5927\u5c0f\u5199\u8f6c\u6362");
+		jLabel1.setText("\u6587\u4ef6\u540d\u5927\u5c0f\u5199\u6279\u91cf\u8f6c\u6362");
 
 		jLabel2.setFont(new java.awt.Font("微软雅黑", 0, 18));
 		jLabel2.setText("\u987a\u5229\u5236\u4f5cV0.1");
@@ -74,10 +74,10 @@ public class FileOperater extends javax.swing.JFrame {
 		});
 
 		jRadioButton1.setFont(new java.awt.Font("微软雅黑", 0, 18));
+		jRadioButton1.setSelected(true);
 		jRadioButton1.setText("\u5927\u5199\u8f6c\u5c0f\u5199");
 		jRadioButton1
 				.setToolTipText("\u5927\u5199\u8f6c\u5c0f\u5199\u5b9e\u4f8b:\u4e00 -> 1;\u5341 -> 10;\u5341\u4e8c -> 12;\u4e09\u5341 -> 30;\u4e00\u767e\u56db\u5341\u4e8c -> 42;\u4e00\u767e\u96f6\u4e8c\u70b9\u4e8c\u4e94 -> 102.25");
-		jRadioButton1.setSelected(true);
 
 		jRadioButton2.setFont(new java.awt.Font("微软雅黑", 0, 18));
 		jRadioButton2.setText("\u5c0f\u5199\u8f6c\u5927\u5199");
@@ -120,14 +120,14 @@ public class FileOperater extends javax.swing.JFrame {
 																		jTextField1, javax.swing.GroupLayout.DEFAULT_SIZE, 649, Short.MAX_VALUE))).addGap(18,
 														18, 18).addComponent(jButton1).addGap(18, 18, 18)))).addGroup(
 								layout.createSequentialGroup().addContainerGap().addComponent(jLabel6).addGap(226, 226, 226).addComponent(jLabel2))).addGap(22,
-						22, 22)).addGroup(layout.createSequentialGroup().addGap(366, 366, 366).addComponent(jLabel1).addContainerGap(370, Short.MAX_VALUE)));
+						22, 22)).addGroup(layout.createSequentialGroup().addGap(346, 346, 346).addComponent(jLabel1).addContainerGap(349, Short.MAX_VALUE)));
 		layout.setVerticalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(
 				layout.createSequentialGroup().addGap(28, 28, 28).addComponent(jLabel1).addGap(18, 18, 18).addGroup(
 						layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE).addComponent(jLabel2).addComponent(jLabel6)).addGap(42, 42, 42)
 						.addGroup(
-								layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE).addComponent(jLabel3).addComponent(jButton1)
-										.addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE,
-												javax.swing.GroupLayout.PREFERRED_SIZE)).addGap(18, 18, 18).addGroup(
+								layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE).addComponent(jLabel3).addComponent(jTextField1,
+										javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+										.addComponent(jButton1)).addGap(18, 18, 18).addGroup(
 								layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE).addComponent(jLabel4).addComponent(jRadioButton1)
 										.addComponent(jRadioButton2)).addGap(18, 18, 18).addGroup(
 								layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addComponent(jLabel5).addComponent(jScrollPane1,
