85d099bf43d174abb06b37e3e5c0ce7cb3ebe2bc
nhjsjmz@gmail.com <nhjsjmz@gmail.com@35b65ada-3664-11de-8fef-1f5ae417a2df>
2011-06-02 07:10:57 +0000

    DEV: add mockito demo.

---
+++ demo/src/main/java/org/usc/demo/mockito/MockitoTest.java
@@ -0,0 +1,32 @@
+package org.usc.demo.mockito;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import java.util.List;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author ShunLi
+ */
+@SuppressWarnings({"rawtypes","unchecked"})
+public class MockitoTest {
+
+    @Test
+    public void test1(){
+        //mock creation
+        List mockedList = mock(List.class);
+
+        //using mock object
+        mockedList.add("one");
+        mockedList.clear();
+
+        //verification
+        verify(mockedList).add(eq("one"));
+        verify(mockedList).clear();
+    }
+
+
+}
