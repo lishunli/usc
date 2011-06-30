b768d6a8a9d91ae6f625cf5539aaf2465fe501d4
nhjsjmz@gmail.com <nhjsjmz@gmail.com@35b65ada-3664-11de-8fef-1f5ae417a2df>
2011-06-30 13:54:11 +0000

    DEV: tuning bat for other comp.

--- mvc-reload/install.bat
+++ mvc-reload/install.bat
@@ -1,9 +1,10 @@
 @echo off
 cd %~dp0
 
-set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_26
-set PATH=%JAVA_HOME%\bin;%PATH%
+rem set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_26
+rem set PATH=%JAVA_HOME%\bin;%PATH%
 
-call mvn clean install -o
+call mvn clean install 
+rem -o
 
 pause
