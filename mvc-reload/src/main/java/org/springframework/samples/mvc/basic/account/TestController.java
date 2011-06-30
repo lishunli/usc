80fd9ec860f52c068662a2715d3a8e8f5d755c18
nhjsjmz@gmail.com <nhjsjmz@gmail.com@35b65ada-3664-11de-8fef-1f5ae417a2df>
2011-06-30 14:02:49 +0000

    DEV: correct file name and location and add config file.

--- mvc-reload/src/main/java/org/springframework/samples/mvc/basic/account/TestController.java
+++ mvc-reload/src/main/java/org/springframework/samples/mvc/basic/account/TestController.java
@@ -4,11 +4,10 @@ import javax.annotation.Resource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.samples.mvc.basic.service.AccessControlListService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.taifook.mtss.mss.acl.service.AccessControlListService;
 
 @Controller
 @RequestMapping(value = "/test")
