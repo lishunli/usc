618bbc73898edd8b46f4d34da7404eb6177e8265
nhjsjmz@gmail.com <nhjsjmz@gmail.com@35b65ada-3664-11de-8fef-1f5ae417a2df>
2011-07-05 09:08:47 +0000

    DEV: add args.

--- mvc-aspect/src/main/java/org/springframework/samples/mvc/basic/aop/Poincut.java
+++ mvc-aspect/src/main/java/org/springframework/samples/mvc/basic/aop/Poincut.java
@@ -2,13 +2,16 @@ package org.springframework.samples.mvc.basic.aop;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.samples.mvc.basic.model.User;
 
 @Aspect
 public class Poincut {
-    @Pointcut("execution(* org.springframework.samples.mvc.basic.service.Userervice.login(..))")
-    public void isLogin() {}
+	@Pointcut(value = "execution(* org.springframework.samples.mvc.basic.service.Userervice.login(user))", argNames = "user")
+	public void isLogin(User user) {
+	}
 
-    @Pointcut("execution(* org.springframework.samples.mvc.basic.service.Userervice.logout(..))")
-    public void isLogout() {}
+	@Pointcut(value = "execution(* org.springframework.samples.mvc.basic.service.Userervice.logout(user))", argNames = "user")
+	public void isLogout(User user) {
+	}
 
 }
--- mvc-aspect/src/main/java/org/springframework/samples/mvc/basic/aop/SystemAdvisor.java
+++ mvc-aspect/src/main/java/org/springframework/samples/mvc/basic/aop/SystemAdvisor.java
@@ -7,20 +7,23 @@ import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.Order;
+import org.springframework.samples.mvc.basic.model.User;
 
 @Aspect
 public class SystemAdvisor {
 	private Logger log = LoggerFactory.getLogger(getClass());
 
-	@After("org.springframework.samples.mvc.basic.aop.Poincut.isLogin()")
+	@After(value = "org.springframework.samples.mvc.basic.aop.Poincut.isLogin()", argNames = "user")
 	@Order(1)
-	public void logLogin(JoinPoint jp) throws Throwable {
+	public void logLogin(JoinPoint jp, User user) throws Throwable {
 		log.info("invoke {} method", jp.getSignature().getName());
+		log.info("{} user login", user.getUsername());
 	}
 
-	@Before("org.springframework.samples.mvc.basic.aop.Poincut.isLogout()")
+	@Before(value = "org.springframework.samples.mvc.basic.aop.Poincut.isLogout()", argNames = "user")
 	@Order(2)
-	public void logLogout(JoinPoint jp) throws Throwable {
+	public void logLogout(JoinPoint jp, User user) throws Throwable {
 		log.info("invoke {} method", jp.getSignature().getName());
+		log.info("{} user logou", user.getUsername());
 	}
 }
