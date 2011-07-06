23a8b3ee14a6f01e27a4523fc9722615c8c8280d
nhjsjmz@gmail.com <nhjsjmz@gmail.com@35b65ada-3664-11de-8fef-1f5ae417a2df>
2011-07-06 07:48:25 +0000

    DEV:  calculate the time spent in controller.

--- mvc-aspect/src/main/java/org/springframework/samples/mvc/basic/aop/SystemAdvisor.java
+++ mvc-aspect/src/main/java/org/springframework/samples/mvc/basic/aop/SystemAdvisor.java
@@ -15,18 +15,30 @@ public class SystemAdvisor {
 	private Logger log = LoggerFactory.getLogger(getClass());
 
 	@After(value = "org.springframework.samples.mvc.basic.aop.SystemPoincut.isCallUserervice(user)", argNames = "user")
-	@Order(1)
-	public void handleCallUserervice(JoinPoint jp, User user) throws Throwable {
+	@Order(20)
+	public void executeUsererviceHandler(JoinPoint jp, User user) throws Throwable {
 		Object[] params = { user.getUsername(), jp.getSignature().getName(), String.format("%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS", new Date()) };
 		log.info("User [{}] {} at {}.", params);
 		// TODO log into db. and ip? user spring security?
 	}
 
 	@After("org.springframework.samples.mvc.basic.aop.SystemPoincut.isCallController()")
-	@Order(2)
-	public void handleCallController(JoinPoint jp) throws Throwable {
+	@Order(10)
+	public void executeControllerHandler(JoinPoint jp) throws Throwable {
 		log.info("Invoke {}.{}()...", jp.getSignature().getDeclaringTypeName(), jp.getSignature().getName());
 		// TODO add args params into log.
 	}
+
+//	@Around("org.springframework.samples.mvc.basic.aop.SystemPoincut.isCallController()")
+//	@Order(11)
+//	public void executeControllerFlow(ProceedingJoinPoint jp) throws Throwable {
+//		long startTime = 0, endTime = 0;
+//		startTime = System.currentTimeMillis();
+//		jp.proceed(); // execute
+//		endTime = System.currentTimeMillis();
+//
+//		Object[] params = { jp.getSignature().getDeclaringTypeName(), jp.getSignature().getName(), endTime - startTime };
+//		log.info("{}; elapsed time for {}() = {}ms ", params);
+//	}
 
 }
