469bae3aa9365ea9c11c364e8c8a7c31f6dcc9e9
lishunli.me@gmail.com <lishunli.me@gmail.com@35b65ada-3664-11de-8fef-1f5ae417a2df>
2011-09-15 06:02:10 +0000

    DEV: 1. upgrade lang3 version.2. correct build error.

--- demo/src/main/java/org/usc/demo/beanutils/test/BeanUtilsTest1.java
+++ demo/src/main/java/org/usc/demo/beanutils/test/BeanUtilsTest1.java
@@ -1,34 +1,32 @@
 package org.usc.demo.beanutils.test;
 
-import java.beans.PropertyDescriptor;
 import java.util.Date;
 import java.util.Map;
 
-import org.apache.commons.beanutils.PropertyUtils;
 import org.usc.demo.beanutils.model.User;
 import org.usc.demo.beanutils.utils.BeanUtils;
 
 /**
  * @author ShunLi
- * 
+ *
  */
 public class BeanUtilsTest1 {
-	public static void main(String[] args) {
-		User user = new User("lishunli", "lishunli", 23, new Date());
+    public static void main(String[] args) {
+        User user = new User("lishunli", "lishunli", 23, new Date());
 
-		try {
-			for (@SuppressWarnings("unused") PropertyDescriptor propertyDescriptor : PropertyUtils.getPropertyDescriptors(user)) {
-				// System.out.println(PropertyUtils.getProperty(user, propertyDescriptor.getName()));
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+//        try {
+//            for (@SuppressWarnings("unused") PropertyDescriptor propertyDescriptor : PropertyUtils.getPropertyDescriptors(user)) {
+//                // System.out.println(PropertyUtils.getProperty(user, propertyDescriptor.getName()));
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
 
-		Map<String, String> result = BeanUtils.convertBeanToMap(user);
+        Map<String, String> result = BeanUtils.convertBeanToMap(user);
 
-		for (Map.Entry<String, String> entry : result.entrySet()) {
-			System.out.println(entry.getKey() + " : " + entry.getValue());
-		}
+        for (Map.Entry<String, String> entry : result.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
 
-	}
+    }
 }
