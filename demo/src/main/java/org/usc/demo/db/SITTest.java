dec0bc963d869a4b2335dbca53576e3ce5475fe6
lishunli.me@gmail.com <lishunli.me@gmail.com@35b65ada-3664-11de-8fef-1f5ae417a2df>
2011-11-04 08:20:07 +0000

    DEV: add two db demo. do like 1. iterator a list(fetch from db.) 2. check something.

---
+++ demo/src/main/java/org/usc/demo/db/SITTest.java
@@ -0,0 +1,48 @@
+package org.usc.demo.db;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+/**
+ *
+ * @author ShunLi
+ */
+public class SITTest {
+    public static void main(String[] args) throws Exception {
+        // Local SZ
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl("jdbc:oracle:thin:@10.100.53.85:1521:cmn");
+        dataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+        dataSource.setUsername("mssapp");
+        dataSource.setPassword("commission");
+
+        SimpleJdbcTemplate jdbcTemplate = new SimpleJdbcTemplate(dataSource);
+        Map<String, Object> paramMap = new HashMap<String, Object>();
+        List<Map<String, Object>> acList = jdbcTemplate.queryForList("SELECT ac_id FROM cc_ac where ac_id like '%-40'", paramMap);
+
+        String sql1 = "SELECT addr_line1 FROM (SELECT clntn.clnt_id, clntn.notf_mtd_typ, clntn.notf_item_cde, cadd.addr_line1, cadd.addr_line2, cadd.addr_line3 FROM cc_clnt_notf_optn clntn INNER JOIN cc_clnt_addr cadd ON cadd.clnt_id = clntn.notf_clnt_id AND cadd.seq_num = clntn.clnt_addr_seq ) clientNotify WHERE clientNotify.clnt_id = (SELECT cl.clnt_id FROM cc_ac ac INNER JOIN cc_clnt cl ON cl.clnt_id = ac.primy_clnt_id WHERE ac.ac_id = :acId ) AND clientNotify.notf_item_cde ='STMT' AND clientNotify.notf_mtd_typ IN ('MAIL','BY_HAND')";
+        String sql2 = "SELECT addr_line1 FROM (SELECT clntn.clnt_id, clntn.notf_mtd_typ, clntn.notf_item_cde, cadd.addr_line1, cadd.addr_line2, cadd.addr_line3 FROM cc_clnt_notf_optn clntn INNER JOIN cc_clnt_addr cadd ON cadd.clnt_id = clntn.notf_clnt_id AND cadd.seq_num = clntn.clnt_addr_seq ) clientNotify WHERE clientNotify.clnt_id = (SELECT ch.clnt_id FROM cc_ac ac INNER JOIN cc_ac_hldr ch ON ch.ac_id = ac.ac_id AND ch.is_primy = 'Y' INNER JOIN cc_clnt c ON c.clnt_id = ch.clnt_id WHERE ac.ac_id = :acId ) AND clientNotify.notf_item_cde ='STMT' AND clientNotify.notf_mtd_typ IN ('MAIL','BY_HAND')";
+
+        for (int i = 0; i < acList.size(); i++) {
+            String acId = acList.get(i).get("AC_ID").toString();
+            paramMap.put("acId", acId);
+            String one = jdbcTemplate.queryForMap(sql1, paramMap).get("addr_line1").toString();
+            String two = jdbcTemplate.queryForMap(sql2, paramMap).get("addr_line1").toString();
+            if (!one.equals(two)) {
+                System.out.println(acId);
+            } else {
+                System.out.print(".");
+                if (i % 100 == 0) {
+                    System.out.println();
+                }
+            }
+
+        }
+
+    }
+
+}
---
+++ demo/src/main/java/org/usc/demo/db/SITTest2.java
@@ -0,0 +1,135 @@
+package org.usc.demo.db;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+/**
+ *
+ * @author ShunLi
+ */
+public class SITTest2 {
+    public static void main(String[] args) throws Exception {
+        // Local SZ
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl("jdbc:oracle:thin:@10.100.53.85:1521:cmn");
+        dataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+        dataSource.setUsername("mssapp");
+        dataSource.setPassword("commission");
+
+        SimpleJdbcTemplate jdbcTemplate = new SimpleJdbcTemplate(dataSource);
+
+        String baseSql = " SELECT DISTINCT user0_.user_id " +
+                " FROM CO_USER user0_ " +
+                " WHERE (EXISTS " +
+                "   (SELECT account1_.AC_ID, " +
+                "     accountexe4_.AE_CDE " +
+                "   FROM CC_AC account1_ " +
+                "   INNER JOIN cc_ae_aces_grp aeaccessgr2_     ON account1_.ae_aces_grp_cde    =aeaccessgr2_.ae_aces_grp_cde " +
+                "   INNER JOIN CC_AE_ACES_GRP_MBR accountexe3_ ON aeaccessgr2_.AE_ACES_GRP_CDE = accountexe3_.AE_ACES_GRP_CDE " +
+                "   INNER JOIN CC_AE accountexe4_              ON accountexe3_.AE_CDE          =accountexe4_.AE_CDE " +
+                "   WHERE accountexe4_.AE_CDE                                                  =user0_.AE_CDE " +
+                "   AND account1_.BUSIN_ACTY_CDE                                               ='STOCK_OPTIONS' " +
+                "   )) " +
+                " AND (user0_.USER_STAT    IN ('ACTIVE' , 'ISSUE' , 'LOCKED')) " +
+                " AND (user0_.UNIT_CDE NOT IN ('0014' , '0016' , '0045' , '0046')) " +
+                " ORDER BY user0_.USER_ID ";
+
+        String searchSql = " SELECT count(*) " +
+                " FROM cc_ac ac " +
+                " inner join cc_ae_aces_grp grp on grp.ae_aces_grp_cde=ac.ae_aces_grp_cde " +
+                " where grp.ae_aces_grp_cde                          in(SELECT ae_aces_grp_cde " +
+                "     FROM CC_AE_ACES_GRP_MBR MBR " +
+                "     WHERE MBR.AE_CDE IN " +
+                "       ( SELECT :userId AS AE_CDE FROM dual " +
+                "       UNION ALL " +
+                "       SELECT ACT_AE_CDE AS AE_CDE " +
+                "       FROM CC_ACT_AE AAE " +
+                "       WHERE AAE.AE_CDE                        = :userId " +
+                "       AND TO_CHAR(AAE.EFF_DATE,'YYYY-MM-DD') <= " +
+                "         (SELECT VAL " +
+                "         FROM MC_SYS_PARAM " +
+                "         WHERE PARAM_KEY = 'SYS_BUS_DATE' " +
+                "         ) " +
+                "       AND ( AAE.EXPR_DATE                   IS NULL " +
+                "       OR TO_CHAR(AAE.EXPR_DATE,'YYYY-MM-DD') > " +
+                "         (SELECT VAL FROM MC_SYS_PARAM WHERE PARAM_KEY = 'SYS_BUS_DATE' " +
+                "         )) " +
+                "       ) " +
+                "     UNION " +
+                "     SELECT DISTINCT GRP.AE_ACES_GRP_CDE " +
+                "     FROM CC_AE_TEAM_HEAD HEAD, " +
+                "       CC_AE_TEAM_MBR MBR, " +
+                "       CC_AE_ACES_GRP_MBR GRP " +
+                "     WHERE HEAD.ENTRP_CDE = MBR.ENTRP_CDE " +
+                "     AND HEAD.AE_TEAM_CDE = MBR.AE_TEAM_CDE " +
+                "     AND MBR.ENTRP_CDE    = GRP.ENTRP_CDE " +
+                "     AND MBR.AE_CDE       = GRP.AE_CDE " +
+                "     AND HEAD.AE_CDE     IN " +
+                "       ( SELECT :userId AS AE_CDE FROM dual " +
+                "       UNION ALL " +
+                "       SELECT ACT_AE_CDE AS AE_CDE " +
+                "       FROM CC_ACT_AE AAE " +
+                "       WHERE AAE.AE_CDE                        = :userId " +
+                "       AND TO_CHAR(AAE.EFF_DATE,'YYYY-MM-DD') <= " +
+                "         (SELECT VAL " +
+                "         FROM MC_SYS_PARAM " +
+                "         WHERE PARAM_KEY = 'SYS_BUS_DATE' " +
+                "         ) " +
+                "       AND ( AAE.EXPR_DATE                   IS NULL " +
+                "       OR TO_CHAR(AAE.EXPR_DATE,'YYYY-MM-DD') > " +
+                "         (SELECT VAL FROM MC_SYS_PARAM WHERE PARAM_KEY = 'SYS_BUS_DATE' " +
+                "         )) " +
+                "       )) " +
+                " AND ac.BUSIN_ACTY_CDE                               = 'STOCK_OPTIONS' ";
+
+        HashMap<String, Object> paramMap = new HashMap<String, Object>();
+        List<Map<String, Object>> userList = jdbcTemplate.queryForList(baseSql, paramMap);
+
+        List<String> query = jdbcTemplate.query(baseSql, new RowMapper<String>() {
+            public String mapRow(ResultSet rs, int rowNum) throws SQLException {
+                return rs.getString(1);
+            }
+        }, paramMap);
+
+        calcMethod(jdbcTemplate, searchSql, paramMap, query);
+
+        for (int i = 0; i < userList.size(); i++) {
+            String userId = userList.get(i).get("user_id").toString();
+
+            paramMap.put("userId", userId);
+
+            long queryForLong = jdbcTemplate.queryForLong(searchSql, paramMap);
+
+            if (queryForLong > 1) {
+                System.out.println(queryForLong + ":" + userId);
+            }
+        }
+    }
+
+    /**
+     * @param jdbcTemplate
+     * @param searchSql
+     * @param paramMap
+     * @param query
+     */
+    private static void calcMethod(SimpleJdbcTemplate jdbcTemplate, String searchSql, HashMap<String, Object> paramMap, List<String> query) {
+        for (String userId : query) {
+            System.out.println(userId);
+            paramMap.put("userId", userId);
+
+            long queryForLong = jdbcTemplate.queryForLong(searchSql, paramMap);
+
+            if (queryForLong > 1) {
+                System.out.println(queryForLong + ":" + userId);
+            }
+        }
+    }
+
+}
