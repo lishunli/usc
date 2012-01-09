e3bcf81db0bd6e313d40a2ac845da5cddb81a89c
lishunli.me@gmail.com <lishunli.me@gmail.com@35b65ada-3664-11de-8fef-1f5ae417a2df>
2012-01-09 07:32:27 +0000

    DEV: first commit.

---
+++ MongoDBDemo1/src/main/java/org/usc/mongodb/DBUtils.java
@@ -0,0 +1,40 @@
+package org.usc.mongodb;
+
+import java.net.UnknownHostException;
+import java.util.Set;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.Mongo;
+import com.mongodb.MongoException;
+
+/**
+ *
+ * @author ShunLi
+ */
+public class DBUtils {
+    public static DBCollection getCollection(String collectionName) throws UnknownHostException, MongoException {
+        Mongo m = new Mongo("localhost", 27017);
+
+        DB db = m.getDB("mydb");
+
+        // show collections
+        Set<String> colls = db.getCollectionNames();
+
+        for (String s : colls) {
+            System.out.println(s);
+        }
+
+        // collection
+        DBCollection coll = db.getCollection(collectionName);
+
+        return coll;
+    }
+
+    public static void showTitle(String title) {
+        System.out.println(StringUtils.center(" " + title + " ", 80, "="));
+    }
+
+}
