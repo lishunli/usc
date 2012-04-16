b43564884cfa43146cc155d0fd75027bebba4e36
lishunli.me@gmail.com <lishunli.me@gmail.com@35b65ada-3664-11de-8fef-1f5ae417a2df>
2012-04-16 10:29:55 +0000

    DEV: add build apache commons svn trunk urls.

---
+++ demo/src/main/java/org/usc/demo/BuildApacheCommonSVNURL.java
@@ -0,0 +1,27 @@
+package org.usc.demo;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author Shunli
+ */
+public class BuildApacheCommonSVNURL {
+    public static void main(String[] args) {
+        List<String> commons = Arrays.asList(
+                "attributes", "bcel", "beanutils", "betwixt", "bsf", "chain", "cli", "codec",
+                "collections", "commons-build", "commons-build-plugin", "commons-nightly",
+                "commons-parent", "commons-sandbox-parent", "commons-site", "commons-skin",
+                "compress", "configuration", "csv", "daemon", "dbcp", "dbutils", "digester",
+                "discovery", "el", "email", "exec", "fileupload", "functor", "httpclient",
+                "io", "jci", "jcs", "jelly", "jexl", "jxpath", "lang", "launcher", "logging",
+                "math", "modeler", "net", "ognl", "pool", "primitives", "proxy", "sanselan",
+                "scxml", "validator", "vfs");
+
+        String templte = "svn checkout http://svn.apache.org/repos/asf/commons/proper/%1$s/trunk %1$s";
+        for (String common : commons) {
+            System.out.println(String.format(templte, common));
+        }
+    }
+}
