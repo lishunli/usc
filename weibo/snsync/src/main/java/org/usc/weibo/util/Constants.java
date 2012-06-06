d07b59f6ba9853357a3bae63bd8cacd4b9506edf
lishunli.me@gmail.com <lishunli.me@gmail.com@35b65ada-3664-11de-8fef-1f5ae417a2df>
2012-06-06 12:06:40 +0000

    tuning, puts some params into cookie.

--- weibo/snsync/src/main/java/org/usc/weibo/util/Constants.java
+++ weibo/snsync/src/main/java/org/usc/weibo/util/Constants.java
@@ -6,15 +6,12 @@ public class Constants {
 	public static final String JDBC_JNDI_YOUXI_WEIBO = "jdbc/xlgame_youxi_weibo";
 
 	public static final String LOG_DIR = Config.getProperty("log_dir");
-	public static final String ACT_DIR = "youxiweibo";
+	public static final String ACT_DIR = "snsync";
 	public static final String SYNC_DATE_FORMATE = "yyyyMMdd";
 
-//	public static final String WEIBO_URL_PREFIX = Config.getProperty("weibo_url_prefix");
-	public static final String CALL_BACK_URL = Config.getProperty("call_back_url");
-	public static final Long CACHE_PERIOD = /* RegUtil.getLong(Config.getProperty("cache_period")) */2 * 60 * 60 * 1000L; // h
+	public static final Long CACHE_PERIOD = 2 * 60 * 60 * 1000L; // h
 	public static final Integer WEIBO_CONTENT_MAX_LENGTH = 140;
 
 	public static final String SERVERLIST = Config.getProperty("serverlist");
-	// public static final Long REFRESH_CACHE_TIME = RegUtil.getLong(Config.getProperty("refresh_cache_time"));
 	public static final String WEIBO_CACHE_PREFIX = "WEIBO_";
 }
