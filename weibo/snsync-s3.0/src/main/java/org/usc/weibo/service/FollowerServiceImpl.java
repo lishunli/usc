8810a3a67e4af25897b41ac748cab60f5f3a914d
lishunli.me@gmail.com <lishunli.me@gmail.com@35b65ada-3664-11de-8fef-1f5ae417a2df>
2012-06-11 08:36:41 +0000

    this branch working at servlet 3.0

---
+++ weibo/snsync-s3.0/src/main/java/org/usc/weibo/service/FollowerServiceImpl.java
@@ -0,0 +1,50 @@
+package org.usc.weibo.service;
+
+import java.util.Map;
+import java.util.WeakHashMap;
+
+import com.xunlei.game.activity.dao.DaoFactory;
+import org.usc.weibo.cache.CacheEntry;
+import org.usc.weibo.dao.FollowerDao;
+import org.usc.weibo.vo.Follower;
+import org.usc.weibo.vo.Provider;
+
+public class FollowerServiceImpl implements FollowerService {
+	private static FollowerDao dao = DaoFactory.getDao(FollowerDao.class);
+	private Map<Long, CacheEntry<Follower>> followerCache = new WeakHashMap<Long, CacheEntry<Follower>>(); // followerId==seqId
+
+	@Override
+	public void addFollower(Follower follower) {
+		dao.addFollower(follower);
+	}
+
+	@Override
+	public void updateFollower(Follower follower) {
+		dao.updateFollower(follower);
+	}
+
+	@Override
+	public Follower findById(Long seqId) {
+		Follower follower = null;
+		CacheEntry<Follower> cacheEntry = followerCache.get(seqId);
+
+		if (cacheEntry != null && !cacheEntry.isExpired()) {
+			follower = cacheEntry.getItem();
+		} else { // no cache or cache is invalid
+			follower = dao.findById(seqId);
+			followerCache.put(seqId, new CacheEntry<Follower>(follower));
+		}
+
+		return follower;
+	}
+
+	@Override
+	public Follower findByUserIdAndAppId(String userId, String appId) {
+		return dao.findByUserIdAndAppId(userId, appId);
+	}
+
+	@Override
+	public Follower findByUserIdAndProvider(String userId, Provider provider) {
+		return dao.findByUserIdAndProvider(userId, provider);
+	}
+}
