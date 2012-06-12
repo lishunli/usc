9839e9ecfd33f6f5e3fbaf3fd25456b7345b065a
lishunli.me@gmail.com <lishunli.me@gmail.com@35b65ada-3664-11de-8fef-1f5ae417a2df>
2012-06-12 15:09:29 +0000

    upgraded

--- trunk/weibo/snsync/src/main/java/org/usc/weibo/util/Constants.java
+++ trunk/weibo/snsync/src/main/java/org/usc/weibo/util/Constants.java
@@ -3,20 +3,20 @@ package org.usc.weibo.util;
 import org.usc.weibo.config.Config;
 
 public class Constants {
-	public static final String JDBC_JNDI_YOUXI_WEIBO = "jdbc/xlgame_youxi_weibo";
+    public static final String JDBC_JNDI_SNSYNC = "jdbc/snsync";
 
-	public static final String LOG_DIR = Config.getProperty("log_dir");
-	public static final String ACT_DIR = "snsync";
-	public static final String SYNC_DATE_FORMATE = "yyyyMMdd";
+    public static final String LOG_DIR = Config.getProperty("log_dir");
+    public static final String ACT_DIR = "snsync";
+    public static final String SYNC_DATE_FORMATE = "yyyyMMdd";
 
-	public static final Long CACHE_PERIOD = 2 * 60 * 60 * 1000L; // h
-	public static final Integer WEIBO_CONTENT_MAX_LENGTH = 140;
+    public static final Long CACHE_PERIOD = 2 * 60 * 60 * 1000L; // h
+    public static final Integer WEIBO_CONTENT_MAX_LENGTH = 140;
 
-	public static final String SERVERLIST = Config.getProperty("serverlist");
-	public static final String WEIBO_CACHE_PREFIX = "WEIBO_";
+    public static final String SERVERLIST = Config.getProperty("serverlist");
+    public static final String WEIBO_CACHE_PREFIX = "WEIBO_";
 
-	public static final String DOMAIN = Config.getProperty("domain");
+    public static final String DOMAIN = Config.getProperty("domain");
 
-	public static final String SNS_SYNC_JOB_CRON = Config.getProperty("sns_sync_job_cron");
+    public static final String SNS_SYNC_JOB_CRON = Config.getProperty("sns_sync_job_cron");
 
 }
--- trunk/weibo/snsync/src/test/java/org/usc/weibo/config/ConfigTest.java
+++ trunk/weibo/snsync/src/test/java/org/usc/weibo/config/ConfigTest.java
@@ -11,9 +11,9 @@ import org.usc.weibo.util.Constants;
  */
 public class ConfigTest {
 
-	@Test
-	public void testGetProperty() {
-		assertEquals("10.11.9.32:11211", Config.getProperty("serverlist"));
-		assertEquals("/usr/local/lishunli/logs/", Constants.LOG_DIR);
-	}
+    @Test
+    public void testGetProperty() {
+         assertEquals("0 0/10 * * * ?", Config.getProperty("sns_sync_job_cron"));
+        assertEquals("/usr/local/lishunli/logs/", Constants.LOG_DIR);
+    }
 }
