2b65cb86de60e95b29e7d3252403fc0693651446
lishunli.me@gmail.com <lishunli.me@gmail.com@35b65ada-3664-11de-8fef-1f5ae417a2df>
2012-06-14 14:23:05 +0000

    refactoring for SAE.

--- trunk/weibo/tencent/QWeiboSDK/src/com/tencent/weibo/utils/QHttpClient.java
+++ trunk/weibo/tencent/QWeiboSDK/src/com/tencent/weibo/utils/QHttpClient.java
@@ -1,136 +1,125 @@
 package com.tencent.weibo.utils;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpStatus;
-import org.apache.commons.httpclient.methods.PostMethod;
-import org.apache.commons.httpclient.methods.multipart.FilePart;
-import org.apache.commons.httpclient.methods.multipart.MultipartRequestEntity;
-import org.apache.commons.httpclient.methods.multipart.Part;
-import org.apache.commons.httpclient.methods.multipart.StringPart;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import com.sina.sae.fetchurl.BinaryData;
 import com.sina.sae.fetchurl.SaeFetchurl;
 import com.tencent.weibo.beans.QParameter;
 
 public class QHttpClient {
+    private static Log log = LogFactory.getLog(QHttpClient.class);
 
-	private static final int CONNECTION_TIMEOUT = 20000;
-	private static Log log = LogFactory.getLog(QHttpClient.class);
+    public QHttpClient() {
 
-	public QHttpClient() {
+    }
 
-	}
+    /**
+     * Using GET method.
+     *
+     * @param url
+     *            The remote URL.
+     * @param queryString
+     *            The query string containing parameters
+     * @return Response string.
+     * @throws Exception
+     */
+    public String httpGet(String url, String queryString) throws Exception {
+        if (queryString != null && !queryString.equals("")) {
+            url += "?" + queryString;
+        }
 
-	/**
-	 * Using GET method.
-	 *
-	 * @param url
-	 *            The remote URL.
-	 * @param queryString
-	 *            The query string containing parameters
-	 * @return Response string.
-	 * @throws Exception
-	 */
-	public String httpGet(String url, String queryString) throws Exception {
-		if (queryString != null && !queryString.equals("")) {
-			url += "?" + queryString;
-		}
+        log.info("httpGet [1]. url = " + url);
 
-		log.info("httpGet [1]. url = " + url);
+        SaeFetchurl fetchUrl = new SaeFetchurl();
+        String responseData = fetchUrl.fetch(url);
 
-		SaeFetchurl fetchUrl = new SaeFetchurl();
-		String responseData = fetchUrl.fetch(url);
+        return responseData;
+    }
 
-		return responseData;
-	}
+    /**
+     * Using POST method.
+     *
+     * @param url
+     *            The remote URL.
+     * @param queryString
+     *            The query string containing parameters
+     * @return Response string.
+     * @throws Exception
+     */
+    public String httpPost(String url, String queryString) throws Exception {
+        queryString = queryString.substring(queryString.lastIndexOf("?") + 1);
+        String[] firstSplit = queryString.split("&");
+        Map<String, String> maps = new HashMap<String, String>();
+        for (String param : firstSplit) {
+            String[] secondeSplit = param.split("=");
+            maps.put(secondeSplit[0], secondeSplit[1]);
+        }
 
-	/**
-	 * Using POST method.
-	 *
-	 * @param url
-	 *            The remote URL.
-	 * @param queryString
-	 *            The query string containing parameters
-	 * @return Response string.
-	 * @throws Exception
-	 */
-	public String httpPost(String url, String queryString) throws Exception {
-		queryString = queryString.substring(queryString.lastIndexOf("?") + 1);
-		String[] firstSplit = queryString.split("&");
-		Map<String, String> maps = new HashMap<String, String>();
-		for (String param : firstSplit) {
-			String[] secondeSplit = param.split("=");
-			maps.put(secondeSplit[0], secondeSplit[1]);
-		}
+        SaeFetchurl fetchUrl = new SaeFetchurl();
+        fetchUrl.setMethod("post");
+        fetchUrl.setPostData(maps);
 
-		SaeFetchurl fetchUrl = new SaeFetchurl();
-		fetchUrl.setMethod("post");
-		fetchUrl.setPostData(maps);
+        String responseData = fetchUrl.fetch(url);
 
-		String responseData = fetchUrl.fetch(url);
+        return responseData;
+    }
 
-		return responseData;
-	}
+    /**
+     * Using POST method with multiParts.
+     *
+     * @param url
+     *            The remote URL.
+     * @param queryString
+     *            The query string containing parameters
+     * @param files
+     *            The list of image files
+     * @return Response string.
+     * @throws Exception
+     */
+    public String httpPostWithFile(String url, String queryString, List<QParameter> files) throws Exception {
+        List<QParameter> listParams = QHttpUtil.getQueryParameters(queryString);
 
-	/**
-	 * Using POST method with multiParts.
-	 *
-	 * @param url
-	 *            The remote URL.
-	 * @param queryString
-	 *            The query string containing parameters
-	 * @param files
-	 *            The list of image files
-	 * @return Response string.
-	 * @throws Exception
-	 */
-	public String httpPostWithFile(String url, String queryString, List<QParameter> files) throws Exception {
+        Map<String, String> maps = new HashMap<String, String>();
+        for (QParameter qParameter : listParams) {
+            maps.put(qParameter.getName(), qParameter.getValue());
+        }
 
-		String responseData = null;
-		url += '?' + queryString;
-		HttpClient httpClient = new HttpClient();
-		PostMethod httpPost = new PostMethod(url);
-		try {
-			List<QParameter> listParams = QHttpUtil.getQueryParameters(queryString);
-			int length = listParams.size() + (files == null ? 0 : files.size());
-			Part[] parts = new Part[length];
-			int i = 0;
-			for (QParameter param : listParams) {
-				parts[i++] = new StringPart(param.getName(), QHttpUtil.formParamDecode(param.getValue()), "UTF-8");
-			}
+        SaeFetchurl fetchUrl = new SaeFetchurl();
+        fetchUrl.setMethod("post");
+        fetchUrl.setPostData(maps);
 
-			for (QParameter param : files) {
-				String filePath = param.getValue();
-				File file = new File(filePath);
-				String name = param.getName();
-				// String fileName = file.getName();
-				// String type = QHttpUtil.getContentType(file);
+        for (QParameter param : files) {
+            BinaryData data = new BinaryData();
 
-				// image/jpeg - image/png
-				parts[i++] = new FilePart(name, file, "image/jpeg", "utf-8");
-			}
+            String filePath = param.getValue();
+            File file = new File(filePath);
 
-			httpPost.setRequestEntity(new MultipartRequestEntity(parts, httpPost.getParams()));
+            FileInputStream stream = new FileInputStream(file);
+            ByteArrayOutputStream out = new ByteArrayOutputStream(1000);
+            byte[] b = new byte[1000];
+            for (int n; (n = stream.read(b)) != -1;) {
+                out.write(b, 0, n);
+            }
+            stream.close();
+            out.close();
 
-			int statusCode = httpClient.executeMethod(httpPost);
-			if (statusCode != HttpStatus.SC_OK) {
-				System.err.println("HttpPost Method failed: " + httpPost.getStatusLine());
-			}
-			responseData = httpPost.getResponseBodyAsString();
-		} catch (Exception e) {
-			throw new Exception(e);
-		} finally {
-			httpPost.releaseConnection();
-			httpClient = null;
-		}
+            data.setInputParameterName(param.getName());
+            data.setFileName(param.getName());
+            data.setPostData(out.toByteArray());
 
-		return responseData;
-	}
+            fetchUrl.setPostData(data);
+        }
 
+        String responseData = fetchUrl.fetch(url);
+
+        return responseData;
+    }
 }
