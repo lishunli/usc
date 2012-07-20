8e060b94c97d88db4a2fc4e5b931c394d1613780
lishunli.me@gmail.com <lishunli.me@gmail.com@35b65ada-3664-11de-8fef-1f5ae417a2df>
2012-07-20 16:42:51 +0000

    tuning.

--- demo/src/main/java/org/usc/demo/RandomTest.java
+++ demo/src/main/java/org/usc/demo/RandomTest.java
@@ -4,28 +4,56 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Random;
+import java.util.Set;
 
 /**
  *
  * @author ShunLi
  */
 public class RandomTest {
+    private static final Random random = new Random();
 
     public static void main(String[] args) {
-        Random random = new Random();
-        System.out.println(random.nextInt(10));
-        System.out.println(random.nextDouble());
+        // System.out.println(random.nextInt(10));
+        // System.out.println(nextDouble);
 
-        Map<String,Double> rateMap = new HashMap<String, Double>() ;
+        // Map<String,Double> rateMap = new HashMap<String, Double>() ;
 
-//        rateMap.
+        // rateMap.
 
+        // List<Double> orignalRates = Arrays.asList(0.1d, 0.2d, 0.3d, 0.4d);
+        List<Double> rates = Arrays.asList(0.1d, 0.3d, 0.6d, 1.0d);
+        double result = rates.get(0);
+        Map<Integer, Integer> count = new HashMap<Integer, Integer>();
 
-        List<Double> rate = Arrays.asList(0.1, 0.2, 0.3, 0.4);
+        for (int i = 0; i < 100000; i++) {
+            double nextDouble = random.nextDouble();
 
+            for (double rate : rates) {
+                if (nextDouble >= rate) {
+                    continue;
+                }
+                result = rate;
+                break;
+            }
+            int index = rates.indexOf(result);
+            // System.out.println(result + ":" + index);
 
+            Integer value = count.get(index);
+            count.put(index, value == null ? 1 : value + 1);
+        }
 
+        System.out.println(count);
+
+        Set<Entry<Integer, Integer>> entrySet = count.entrySet();
+        double sum = 0;
+        for (Entry<Integer, Integer> entry : entrySet) {
+            sum += entry.getValue();
+        }
+        for (Entry<Integer, Integer> entry : entrySet) {
+            System.out.println(entry.getKey() + ":" + entry.getValue() / sum);
+        }
     }
-
 }
