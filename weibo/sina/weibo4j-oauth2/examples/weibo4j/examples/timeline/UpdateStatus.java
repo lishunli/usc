d40915d60626a42d65e520be59fb227737e39d28
lishunli.me@gmail.com <lishunli.me@gmail.com@35b65ada-3664-11de-8fef-1f5ae417a2df>
2012-08-15 12:03:18 +0000

    tunging to act env.

--- weibo/sina/weibo4j-oauth2/examples/weibo4j/examples/timeline/UpdateStatus.java
+++ weibo/sina/weibo4j-oauth2/examples/weibo4j/examples/timeline/UpdateStatus.java
@@ -9,8 +9,8 @@ import weibo4j.model.WeiboException;
 public class UpdateStatus {
 
 	public static void main(String[] args) {
-		String access_token = args[0];
-		String statuses = args[1];
+		String access_token = "2.00WP3ohBC9fwHBbead892d05kL4p2C";// args[0];
+		String statuses = "test OAuth2 In Sina Weibo";// args[1];
 		Weibo weibo = new Weibo();
 		weibo.setToken(access_token);
 		Timeline tm = new Timeline();
@@ -19,6 +19,7 @@ public class UpdateStatus {
 			Log.logInfo(status.toString());
 		} catch (WeiboException e) {
 			e.printStackTrace();
-		}	}
+		}
+	}
 
 }
