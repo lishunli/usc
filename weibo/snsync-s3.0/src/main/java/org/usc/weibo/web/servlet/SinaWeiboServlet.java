f562a1931518185f73586c56df1cd7dd97519b53
lishunli.me@gmail.com <lishunli.me@gmail.com@35b65ada-3664-11de-8fef-1f5ae417a2df>
2012-08-28 04:35:16 +0000

    add manual sync function.

--- weibo/snsync-s3.0/src/main/java/org/usc/weibo/web/servlet/SinaWeiboServlet.java
+++ weibo/snsync-s3.0/src/main/java/org/usc/weibo/web/servlet/SinaWeiboServlet.java
@@ -29,95 +29,95 @@ import com.xunlei.game.activity.vo.JsonRtn;
  */
 @WebServlet(urlPatterns = "/sinaweibo")
 public class SinaWeiboServlet extends SnsBaseServlet {
-	private static final long serialVersionUID = -2910525161848529312L;
+    private static final long serialVersionUID = -2910525161848529312L;
 
-	protected static Logger log = LogFactory.getLogger(Constants.LOG_DIR, Constants.ACT_DIR, "sinaweibo");
+    protected static Logger log = LogFactory.getLogger(Constants.LOG_DIR, Constants.ACT_DIR, "sinaweibo");
 
-	private Application app;
-	private Oauth oauth;
+    private Application app;
+    private Oauth oauth;
 
-	public void auth(HttpServletRequest request, HttpServletResponse response) {
-		try {
-			synchronized (this) {
-				app = appService.randGetOneApp(Provider.SINA);
-				oauth = new Oauth();
-			}
+    public void auth(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            synchronized (this) {
+                app = appService.randGetOneApp(Provider.SINA);
+                oauth = new Oauth();
+            }
 
-			if (app == null) {
-				log.info("no SINA weibo application, please check");
-				return;
-			}
+            if (app == null) {
+                log.info("no SINA weibo application, please check");
+                return;
+            }
 
-			response.sendRedirect(oauth.authorize(app.getOauthConsumerKey(), Constants.CONF.getValue(Constants.SINA_WEIBO_CALL_BACK_REDIRECT_URI_PROP), "code"));
+            response.sendRedirect(oauth.authorize(app.getOauthConsumerKey(), Constants.CONF.getValue(Constants.SINA_WEIBO_CALL_BACK_REDIRECT_URI_PROP), "code"));
 
-		} catch (Exception e) {
-			log.error("auth-error: ", e);
-			super.outputRtn(request, response, new JsonRtn<Object>(-1, "网络超时，请稍后重试！").toJsonString());
-		}
-	}
+        } catch (Exception e) {
+            log.error("auth-error: ", e);
+            super.outputRtn(request, response, new JsonRtn<Object>(-1, "网络超时，请稍后重试！").toJsonString());
+        }
+    }
 
-	public void callBack(HttpServletRequest request, HttpServletResponse response) {
-		try {
-			String code = request.getParameter("code");
+    public void callBack(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            String code = request.getParameter("code");
 
-			String appId = app.getAppId();
-			String clientId = app.getOauthConsumerKey();
-			String clientSecret = app.getOauthConsumerSecret();
-			String redirectUri = Constants.CONF.getValue(Constants.SINA_WEIBO_CALL_BACK_REDIRECT_URI_PROP);
+            String appId = app.getAppId();
+            String clientId = app.getOauthConsumerKey();
+            String clientSecret = app.getOauthConsumerSecret();
+            String redirectUri = Constants.CONF.getValue(Constants.SINA_WEIBO_CALL_BACK_REDIRECT_URI_PROP);
 
-			oauth.authorize(clientId, redirectUri, "code");
-			AccessToken accessToken = oauth.getAccessTokenByCode(clientId, clientSecret, redirectUri, code);
+            oauth.authorize(clientId, redirectUri, "code");
+            AccessToken accessToken = oauth.getAccessTokenByCode(clientId, clientSecret, redirectUri, code);
 
-			System.out.println("Got access token.");
-			System.out.println(accessToken);
+            System.out.println("Got access token.");
+            System.out.println(accessToken);
 
-			String access_token = accessToken.getAccessToken();
-			String userId = accessToken.getUid();
-			System.out.println("Access token: " + access_token);
+            String access_token = accessToken.getAccessToken();
+            String userId = accessToken.getUid();
+            System.out.println("Access token: " + access_token);
 
-			Weibo weibo = new Weibo();
-			weibo.setToken(access_token);
-			Users um = new Users();
-			User user = um.showUserById(userId);
+            Weibo weibo = new Weibo();
+            weibo.setToken(access_token);
+            Users um = new Users();
+            User user = um.showUserById(userId);
 
-			Follower follower = new Follower();
-			follower.setAppId(appId);
-			follower.setUserId(userId);
-			follower.setUserName(user.getName());
-			follower.setToken(access_token);
-			follower.setVerifier(code);
+            Follower follower = new Follower();
+            follower.setAppId(appId);
+            follower.setUserId(userId);
+            follower.setUserName(user.getName());
+            follower.setToken(access_token);
+            follower.setVerifier(code);
 
-			Status lastWeiboContent = user.getStatus();
-			if (lastWeiboContent != null) {
-				follower.setLastId(lastWeiboContent.getId());
-				follower.setLastTimeStamp(lastWeiboContent.getCreatedAt().getTime());
-			}
+            Status lastWeiboContent = user.getStatus();
+            if (lastWeiboContent != null) {
+                follower.setLastId(lastWeiboContent.getId());
+                follower.setLastTimeStamp(lastWeiboContent.getCreatedAt().getTime());
+            }
 
-			Follower model = followerService.findByUserIdAndProvider(userId, AppUtil.getProvider(appId));
-			if (model != null) {
-				model.setAppId(appId);
-				model.setToken(follower.getToken());
-				model.setVerifier(code);
-				model.setLastId(follower.getLastId());
-				model.setLastTimeStamp(follower.getLastTimeStamp());
-				followerService.updateFollower(model);
-			} else {
-				followerService.addFollower(follower);
-				model = followerService.findByUserIdAndAppId(userId, appId);
-			}
+            Follower model = followerService.findByUserIdAndProvider(userId, AppUtil.getProvider(appId));
+            if (model != null) {
+                model.setAppId(appId);
+                model.setToken(follower.getToken());
+                model.setVerifier(code);
+                // model.setLastId(follower.getLastId());
+                // model.setLastTimeStamp(follower.getLastTimeStamp());
+                followerService.updateFollower(model);
+            } else {
+                followerService.addFollower(follower);
+                model = followerService.findByUserIdAndAppId(userId, appId);
+            }
 
-			SinaWeiboCache.putWeibo(model.getSeqId(), weibo);
+            SinaWeiboCache.putWeibo(model.getSeqId(), weibo);
 
-			super.setCookie(request, response, LEFT_FOLLOWER_COOKIE_NAME, model.getSeqId().toString(), -1);
-			log.info("callBack successly " + appId + " access token, followerId=" + model.getSeqId());
+            super.setCookie(request, response, LEFT_FOLLOWER_COOKIE_NAME, model.getSeqId().toString(), -1);
+            log.info("callBack successly " + appId + " access token, followerId=" + model.getSeqId());
 
-			String path = request.getContextPath();
-			String basePath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + path + "/";
-			response.sendRedirect(basePath + "sns?action=sync");
+            String path = request.getContextPath();
+            String basePath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + path + "/";
+            response.sendRedirect(basePath + "sns?action=sync");
 
-		} catch (Exception e) {
-			log.error("callBack-error: ", e);
-			super.outputRtn(request, response, new JsonRtn<Object>(-1, "网络超时，请稍后重试！").toJsonString());
-		}
-	}
+        } catch (Exception e) {
+            log.error("callBack-error: ", e);
+            super.outputRtn(request, response, new JsonRtn<Object>(-1, "网络超时，请稍后重试！").toJsonString());
+        }
+    }
 }
--- weibo/snsync-s3.0/src/main/java/org/usc/weibo/web/servlet/SnsServlet.java
+++ weibo/snsync-s3.0/src/main/java/org/usc/weibo/web/servlet/SnsServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
+import org.usc.weibo.service.weibo.WeiboSyncFactory;
 import org.usc.weibo.util.Constants;
 import org.usc.weibo.vo.Relation;
 
@@ -20,59 +21,72 @@ import com.xunlei.game.activity.vo.JsonRtn;
  */
 @WebServlet(urlPatterns = "/sns")
 public class SnsServlet extends SnsBaseServlet {
-	private static final long serialVersionUID = -1477065409747225124L;
-	protected static Logger log = LogFactory.getLogger(Constants.LOG_DIR, Constants.ACT_DIR, "sns");
+    private static final long serialVersionUID = -1477065409747225124L;
+    protected static Logger log = LogFactory.getLogger(Constants.LOG_DIR, Constants.ACT_DIR, "sns");
 
-	public void sync(HttpServletRequest request, HttpServletResponse response) {
-		try {
-			Long leftFollowerId = RegUtil.getLong(super.getCookie(request, response, LEFT_FOLLOWER_COOKIE_NAME));
-			Long rightFollowerId = RegUtil.getLong(super.getCookie(request, response, RIGHT_FOLLOWER_COOKIE_NAME));
+    public void sync(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            Long leftFollowerId = RegUtil.getLong(super.getCookie(request, response, LEFT_FOLLOWER_COOKIE_NAME));
+            Long rightFollowerId = RegUtil.getLong(super.getCookie(request, response, RIGHT_FOLLOWER_COOKIE_NAME));
 
-			if (leftFollowerId == null || rightFollowerId == null) {
-				log.info("sync-no-cookie,please auth both left and right!" + leftFollowerId + "," + rightFollowerId);
-			} else {
-				Relation originRelation = relationService.findByTwoWayFollowers(leftFollowerId, rightFollowerId);
+            if (leftFollowerId == null || rightFollowerId == null) {
+                log.info("sync-no-cookie,please auth both left and right!" + leftFollowerId + "," + rightFollowerId);
+            } else {
+                Relation originRelation = relationService.findByTwoWayFollowers(leftFollowerId, rightFollowerId);
 
-				if (originRelation == null) {
-					Relation relation = new Relation(leftFollowerId, rightFollowerId); // default is two-way
-					relationService.addRelation(relation);
-				} else {
-					// update , now is no-op, maybe will change th two way property.
-					// originRelation.setIsTwoWay(0);
-					// relationService.updateRelation(originRelation);
-				}
-			}
+                if (originRelation == null) {
+                    Relation relation = new Relation(leftFollowerId, rightFollowerId); // default is two-way
+                    relationService.addRelation(relation);
+                } else {
+                    // update , now is no-op, maybe will change th two way property.
+                    // originRelation.setIsTwoWay(0);
+                    // relationService.updateRelation(originRelation);
+                }
+            }
 
-			log.info("sync success:" + leftFollowerId + "," + rightFollowerId + "," + ServerUtil.getRealIp(request));
-			String path = request.getContextPath();
-			String basePath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + path + "/";
-			response.sendRedirect(basePath);
-		} catch (Exception e) {
-			log.error("sync-error: ", e);
-			super.outputRtn(request, response, new JsonRtn<Object>(-1, "网络超时，请稍后重试！").toJsonString());
-		}
-	}
-	public void cansync(HttpServletRequest request, HttpServletResponse response) {
-		try {
-			Long leftFollowerId = RegUtil.getLong(super.getCookie(request, response, LEFT_FOLLOWER_COOKIE_NAME));
-			Long rightFollowerId = RegUtil.getLong(super.getCookie(request, response, RIGHT_FOLLOWER_COOKIE_NAME));
+            log.info("sync success:" + leftFollowerId + "," + rightFollowerId + "," + ServerUtil.getRealIp(request));
+            String path = request.getContextPath();
+            String basePath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + path + "/";
+            response.sendRedirect(basePath);
+        } catch (Exception e) {
+            log.error("sync-error: ", e);
+            super.outputRtn(request, response, new JsonRtn<Object>(-1, "网络超时，请稍后重试！").toJsonString());
+        }
+    }
 
-			if (leftFollowerId == null && rightFollowerId == null) {
-				log.info("cansync-no-cookie,please auth both left and right!" + leftFollowerId + rightFollowerId);
-			} else {
-				relationService.cancelRelation(leftFollowerId, rightFollowerId);
-				super.delCookie(response, LEFT_FOLLOWER_COOKIE_NAME);
-				super.delCookie(response, RIGHT_FOLLOWER_COOKIE_NAME);
+    public void cansync(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            Long leftFollowerId = RegUtil.getLong(super.getCookie(request, response, LEFT_FOLLOWER_COOKIE_NAME));
+            Long rightFollowerId = RegUtil.getLong(super.getCookie(request, response, RIGHT_FOLLOWER_COOKIE_NAME));
 
-				log.info("cansync success:" + leftFollowerId + "," + rightFollowerId + "," + ServerUtil.getRealIp(request));
-			}
+            if (leftFollowerId == null && rightFollowerId == null) {
+                log.info("cansync-no-cookie,please auth both left and right!" + leftFollowerId + rightFollowerId);
+            } else {
+                relationService.cancelRelation(leftFollowerId, rightFollowerId);
+                super.delCookie(response, LEFT_FOLLOWER_COOKIE_NAME);
+                super.delCookie(response, RIGHT_FOLLOWER_COOKIE_NAME);
 
-			String path = request.getContextPath();
-			String basePath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + path + "/";
-			response.sendRedirect(basePath);
-		} catch (Exception e) {
-			log.error("cansync-error: ", e);
-			super.outputRtn(request, response, new JsonRtn<Object>(-1, "网络超时，请稍后重试！").toJsonString());
-		}
-	}
+                log.info("cansync success:" + leftFollowerId + "," + rightFollowerId + "," + ServerUtil.getRealIp(request));
+            }
+
+            String path = request.getContextPath();
+            String basePath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + path + "/";
+            response.sendRedirect(basePath);
+        } catch (Exception e) {
+            log.error("cansync-error: ", e);
+            super.outputRtn(request, response, new JsonRtn<Object>(-1, "网络超时，请稍后重试！").toJsonString());
+        }
+    }
+
+    public void doAction(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            WeiboSyncFactory.sync();
+            log.info("doAction-success");
+            super.outputRtn(request, response, new JsonRtn<Object>(0, "成功").toJsonString());
+        } catch (Exception e) {
+            log.error("doAction-error: ", e);
+            super.outputRtn(request, response, new JsonRtn<Object>(-1, "网络超时，请稍后重试！").toJsonString());
+        }
+    }
+
 }
