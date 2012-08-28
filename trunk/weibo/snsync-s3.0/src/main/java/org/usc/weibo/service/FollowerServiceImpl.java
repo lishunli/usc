5c54ba098e2b1f986689a1effbfc63d61a45365e
lishunli.me@gmail.com <lishunli.me@gmail.com@35b65ada-3664-11de-8fef-1f5ae417a2df>
2012-08-28 04:35:16 +0000

    add manual sync function.

--- trunk/weibo/snsync-s3.0/src/main/java/org/usc/weibo/service/FollowerServiceImpl.java
+++ trunk/weibo/snsync-s3.0/src/main/java/org/usc/weibo/service/FollowerServiceImpl.java
@@ -3,48 +3,52 @@ package org.usc.weibo.service;
 import java.util.Map;
 import java.util.WeakHashMap;
 
-import com.xunlei.game.activity.dao.DaoFactory;
 import org.usc.weibo.cache.CacheEntry;
 import org.usc.weibo.dao.FollowerDao;
 import org.usc.weibo.vo.Follower;
 import org.usc.weibo.vo.Provider;
 
+import com.xunlei.game.activity.dao.DaoFactory;
+
 public class FollowerServiceImpl implements FollowerService {
-	private static FollowerDao dao = DaoFactory.getDao(FollowerDao.class);
-	private Map<Long, CacheEntry<Follower>> followerCache = new WeakHashMap<Long, CacheEntry<Follower>>(); // followerId==seqId
+    private static FollowerDao dao = DaoFactory.getDao(FollowerDao.class);
+    @SuppressWarnings("unused")
+    private Map<Long, CacheEntry<Follower>> followerCache = new WeakHashMap<Long, CacheEntry<Follower>>(); // followerId==seqId
 
-	@Override
-	public void addFollower(Follower follower) {
-		dao.addFollower(follower);
-	}
+    @Override
+    public void addFollower(Follower follower) {
+        dao.addFollower(follower);
+    }
 
-	@Override
-	public void updateFollower(Follower follower) {
-		dao.updateFollower(follower);
-	}
+    @Override
+    public void updateFollower(Follower follower) {
+        dao.updateFollower(follower);
+    }
 
-	@Override
-	public Follower findById(Long seqId) {
-		Follower follower = null;
-		CacheEntry<Follower> cacheEntry = followerCache.get(seqId);
+    @Override
+    public Follower findById(Long seqId) {
+        // Follower follower = null;
+        // CacheEntry<Follower> cacheEntry = followerCache.get(seqId);
+        //
+        // if (cacheEntry != null && !cacheEntry.isExpired()) {
+        // follower = cacheEntry.getItem();
+        // } else { // no cache or cache is invalid
+        // follower = dao.findById(seqId);
+        // followerCache.put(seqId, new CacheEntry<Follower>(follower));
+        // }
+        //
+        // return follower;
 
-		if (cacheEntry != null && !cacheEntry.isExpired()) {
-			follower = cacheEntry.getItem();
-		} else { // no cache or cache is invalid
-			follower = dao.findById(seqId);
-			followerCache.put(seqId, new CacheEntry<Follower>(follower));
-		}
+        return dao.findById(seqId);
+    }
 
-		return follower;
-	}
+    @Override
+    public Follower findByUserIdAndAppId(String userId, String appId) {
+        return dao.findByUserIdAndAppId(userId, appId);
+    }
 
-	@Override
-	public Follower findByUserIdAndAppId(String userId, String appId) {
-		return dao.findByUserIdAndAppId(userId, appId);
-	}
-
-	@Override
-	public Follower findByUserIdAndProvider(String userId, Provider provider) {
-		return dao.findByUserIdAndProvider(userId, provider);
-	}
+    @Override
+    public Follower findByUserIdAndProvider(String userId, Provider provider) {
+        return dao.findByUserIdAndProvider(userId, provider);
+    }
 }
--- trunk/weibo/snsync-s3.0/src/main/java/org/usc/weibo/service/weibo/WeiboSyncFactory.java
+++ trunk/weibo/snsync-s3.0/src/main/java/org/usc/weibo/service/weibo/WeiboSyncFactory.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.usc.weibo.service.ApplicationService;
+import org.usc.weibo.service.FollowerService;
+import org.usc.weibo.service.RelationService;
 import org.usc.weibo.util.Constants;
 import org.usc.weibo.vo.Follower;
 import org.usc.weibo.vo.Pair;
 import org.usc.weibo.vo.Provider;
+import org.usc.weibo.vo.Relation;
 import org.usc.weibo.vo.WeiboContent;
 
 import com.xunlei.game.activity.log.LogFactory;
@@ -21,6 +24,8 @@ public class WeiboSyncFactory {
     private static Logger log = LogFactory.getLoggerDaily(Constants.LOG_DIR, Constants.ACT_DIR, "sendWeiboJob");
 
     protected static ApplicationService appService = ServiceFactory.getService(ApplicationService.class);
+    protected static FollowerService followerService = ServiceFactory.getService(FollowerService.class);
+    protected static RelationService relationService = ServiceFactory.getService(RelationService.class);
 
     public static void weiboSync(Follower leftFollower, Follower rightFollower, int isTwoWay) throws Exception {
         WeiboService leftWeiboService = buildWeiboService(leftFollower);
@@ -40,6 +45,30 @@ public class WeiboSyncFactory {
             log.info("please check the relation's two way param.");
         }
 
+    }
+
+    /**
+     * common method to sync weibo. job + manual
+     *
+     *
+     * @throws Exception
+     */
+    public static void sync() throws Exception {
+        List<Relation> allRelations = relationService.findAll();
+
+        for (Relation realation : allRelations) {
+            log.info("hanlde " + realation);
+
+            Follower leftFollower = followerService.findById(realation.getLeftFollowerId());
+            Follower rightFollower = followerService.findById(realation.getRightFollowerId());
+
+            if (leftFollower == null || rightFollower == null) {
+                log.info("no follower" + leftFollower + rightFollower);
+                return;
+            }
+
+            weiboSync(leftFollower, rightFollower, realation.getIsTwoWay());
+        }
     }
 
     /**
