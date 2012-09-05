a4e22cc9ef2c41194ced20221bbbf24a4166736c
lishunli.me@gmail.com <lishunli.me@gmail.com@35b65ada-3664-11de-8fef-1f5ae417a2df>
2012-09-05 02:04:21 +0000

    reverse weibo bcz now order by send time desc(first record is last weibo).

--- trunk/weibo/snsync-s3.0/src/main/java/org/usc/weibo/service/weibo/SinaWeiboServiceImpl.java
+++ trunk/weibo/snsync-s3.0/src/main/java/org/usc/weibo/service/weibo/SinaWeiboServiceImpl.java
@@ -1,6 +1,7 @@
 package org.usc.weibo.service.weibo;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
@@ -27,120 +28,122 @@ import com.xunlei.game.activity.utils.RegUtil;
  * @author Shunli
  */
 public class SinaWeiboServiceImpl extends AbstractWeiboService implements WeiboService {
-	private static final String SINA_FROM_ID_TO_URL = "http://api.t.sina.com.cn/%s/statuses/%s";
-	private static final int SHORT_URL_MAX_LENGTH = 10;
+    private static final String SINA_FROM_ID_TO_URL = "http://api.t.sina.com.cn/%s/statuses/%s";
+    private static final int SHORT_URL_MAX_LENGTH = 10;
 
-	public SinaWeiboServiceImpl(Follower follower) {
-		super(follower);
-	}
+    public SinaWeiboServiceImpl(Follower follower) {
+        super(follower);
+    }
 
-	@Override
-	public Pair<Pair<String, Long>, List<WeiboContent>> read() throws Exception {
-		Long sinaFollowerId = follower.getSeqId();
-		Weibo weibo = SinaWeiboCache.getWeibo(sinaFollowerId, true);
+    @Override
+    public Pair<Pair<String, Long>, List<WeiboContent>> read() throws Exception {
+        Long sinaFollowerId = follower.getSeqId();
+        Weibo weibo = SinaWeiboCache.getWeibo(sinaFollowerId, true);
 
-		if (weibo == null) {
-			log.info("no sina weibo,please check it " + sinaFollowerId);
-			throw new Exception("no sina cache");
-		}
+        if (weibo == null) {
+            log.info("no sina weibo,please check it " + sinaFollowerId);
+            throw new Exception("no sina cache");
+        }
 
-		Paging pag = new Paging();
-		pag.setSinceId(StringUtils.isNotEmpty(follower.getLastId()) ? RegUtil.getLong(follower.getLastId()) : 1L);
-		pag.setCount(100);
+        Paging pag = new Paging();
+        pag.setSinceId(StringUtils.isNotEmpty(follower.getLastId()) ? RegUtil.getLong(follower.getLastId()) : 1L);
+        pag.setCount(100);
 
-		Timeline tm = new Timeline();
-		StatusWapper statusWapper = tm.getUserTimelineByUid(follower.getUserId(), pag, 0, 0);
-		List<Status> statuses = statusWapper.getStatuses();
+        Timeline tm = new Timeline();
+        StatusWapper statusWapper = tm.getUserTimelineByUid(follower.getUserId(), pag, 0, 0);
+        List<Status> statuses = statusWapper.getStatuses();
 
-		String lastId = follower.getLastId();
-		Long lastTimeStamp = follower.getLastTimeStamp();
-		List<WeiboContent> weiboContents = null;
+        String lastId = follower.getLastId();
+        Long lastTimeStamp = follower.getLastTimeStamp();
+        List<WeiboContent> weiboContents = null;
 
-		if (statuses != null && !statuses.isEmpty()) {
-			weiboContents = new ArrayList<WeiboContent>(statuses.size());
+        if (statuses != null && !statuses.isEmpty()) {
+            weiboContents = new ArrayList<WeiboContent>(statuses.size());
 
-			for (Status status : statuses) {
-				if (!syncRecordService.isSynchronized(sinaFollowerId, status.getId())) {
-					Status retweetedStatus = status.getRetweetedStatus();
+            for (Status status : statuses) {
+                if (!syncRecordService.isSynchronized(sinaFollowerId, status.getId())) {
+                    Status retweetedStatus = status.getRetweetedStatus();
 
-					String text = status.getText();
-					String pic = status.getOriginalPic();
-					if (retweetedStatus == null) {
-						// TODO-Shunli: maybe sometimes can throw exception since different lenght of short url between sina and tx weibo.
-						weiboContents.add(new WeiboContent(status.getId(), text, pic, status.getCreatedAt().getTime()));
-					} else {
-						String retweetedId = retweetedStatus.getId();
-						String from = " //来自微博@" + retweetedStatus.getUser().getName() + " ";
-						String linkUrl = String.format(SINA_FROM_ID_TO_URL, retweetedStatus.getUser().getId(), retweetedId);
-						int maxWidth = Constants.WEIBO_CONTENT_MAX_LENGTH - WeiboUtil.length(from) - SHORT_URL_MAX_LENGTH;
+                    String text = status.getText();
+                    String pic = status.getOriginalPic();
+                    if (retweetedStatus == null) {
+                        // TODO-Shunli: maybe sometimes can throw exception since different lenght of short url between sina and tx weibo.
+                        weiboContents.add(new WeiboContent(status.getId(), text, pic, status.getCreatedAt().getTime()));
+                    } else {
+                        String retweetedId = retweetedStatus.getId();
+                        String from = " //来自微博@" + retweetedStatus.getUser().getName() + " ";
+                        String linkUrl = String.format(SINA_FROM_ID_TO_URL, retweetedStatus.getUser().getId(), retweetedId);
+                        int maxWidth = Constants.WEIBO_CONTENT_MAX_LENGTH - WeiboUtil.length(from) - SHORT_URL_MAX_LENGTH;
 
-						// keep the more real original weibo text.
-						String retweetedText = retweetedStatus.getText();
-						if (WeiboUtil.length(retweetedText) <= maxWidth) {
-							retweetedText = retweetedText + from + linkUrl;
-						} else {
-							text = WeiboUtil.abbreviate(text, maxWidth) + from + linkUrl;
-						}
+                        // keep the more real original weibo text.
+                        String retweetedText = retweetedStatus.getText();
+                        if (WeiboUtil.length(retweetedText) <= maxWidth) {
+                            retweetedText = retweetedText + from + linkUrl;
+                        } else {
+                            text = WeiboUtil.abbreviate(text, maxWidth) + from + linkUrl;
+                        }
 
-						WeiboContent retweetedContent = new WeiboContent(retweetedId, retweetedText, retweetedStatus.getOriginalPic(), retweetedStatus.getCreatedAt().getTime());
-						weiboContents.add(new WeiboContent(status.getId(), text, "", status.getCreatedAt().getTime(), retweetedContent));
-					}
-				} else {
-					log.info("skip weibo " + status);
-				}
-			}
+                        WeiboContent retweetedContent = new WeiboContent(retweetedId, retweetedText, retweetedStatus.getOriginalPic(), retweetedStatus.getCreatedAt().getTime());
+                        weiboContents.add(new WeiboContent(status.getId(), text, "", status.getCreatedAt().getTime(), retweetedContent));
+                    }
+                } else {
+                    log.info("skip weibo " + status);
+                }
+            }
 
-			// // asc sort weibo content by id.
-			// Collections.sort(weiboContents);
+            // // asc sort weibo content by id.
+            // Collections.sort(weiboContents);
 
-			if (weiboContents.size() > 0) {
-				// lastId = max = last list
-				WeiboContent lastWeiboContent = weiboContents.get(0);
-				lastId = lastWeiboContent.getId();
-				lastTimeStamp = lastWeiboContent.getTimeStamp();
-			}
-		}
+            if (weiboContents.size() > 0) {
+                // lastId = max = last list
+                WeiboContent lastWeiboContent = weiboContents.get(0);
+                lastId = lastWeiboContent.getId();
+                lastTimeStamp = lastWeiboContent.getTimeStamp();
+            }
+        }
 
-		log.info("SinaWeiboServiceImpl.read()" + weiboContents);
+        log.info("SinaWeiboServiceImpl.read()" + weiboContents);
 
-		return new Pair<Pair<String, Long>, List<WeiboContent>>(new Pair<String, Long>(lastId, lastTimeStamp), weiboContents);
-	}
+        // reverse weibo bcz now order by send time desc(first record is last weibo).
+        Collections.reverse(weiboContents);
+        return new Pair<Pair<String, Long>, List<WeiboContent>>(new Pair<String, Long>(lastId, lastTimeStamp), weiboContents);
+    }
 
-	@Override
-	public void write(List<WeiboContent> weiboContents) throws Exception {
+    @Override
+    public void write(List<WeiboContent> weiboContents) throws Exception {
 
-		/**
-		 * bcoz weibo limit too much, so write to sina weibo handle in later.
-		 */
-		Long sinaFollowerId = follower.getSeqId();
-		Weibo weibo = SinaWeiboCache.getWeibo(sinaFollowerId, true);
+        /**
+         * bcoz weibo limit too much, so write to sina weibo handle in later.
+         */
+        Long sinaFollowerId = follower.getSeqId();
+        Weibo weibo = SinaWeiboCache.getWeibo(sinaFollowerId, true);
 
-		if (weibo == null) {
-			log.info("no sina weibo,please check it " + sinaFollowerId);
-			throw new Exception("no sina cache");
-		}
+        if (weibo == null) {
+            log.info("no sina weibo,please check it " + sinaFollowerId);
+            throw new Exception("no sina cache");
+        }
 
-		if (weiboContents != null) {
-			for (WeiboContent weiboContent : weiboContents) {
-				Status result = null;
-				Timeline tm = new Timeline();
+        if (weiboContents != null) {
+            for (WeiboContent weiboContent : weiboContents) {
+                Status result = null;
+                Timeline tm = new Timeline();
 
-				if (StringUtils.isBlank(weiboContent.getOriginalPic())) {
-					result = tm.UpdateStatus(weiboContent.getText());
-				} else {
-					result = tm.uploadStatus(weiboContent.getText(), weiboContent.getOriginalPic());
-				}
+                if (StringUtils.isBlank(weiboContent.getOriginalPic())) {
+                    result = tm.UpdateStatus(weiboContent.getText());
+                } else {
+                    result = tm.uploadStatus(weiboContent.getText(), weiboContent.getOriginalPic());
+                }
 
-				if (result != null) {
-					// i want lazy... + Sleep
-					syncRecordService.addSyncRecord(new SyncRecord(sinaFollowerId, result.getId(), DateUtil.formatDate(new Date(), Constants.SYNC_DATE_FORMATE)));
-					Thread.sleep(5000);
-				}
+                if (result != null) {
+                    // i want lazy... + Sleep
+                    syncRecordService.addSyncRecord(new SyncRecord(sinaFollowerId, result.getId(), DateUtil.formatDate(new Date(), Constants.SYNC_DATE_FORMATE)));
+                    Thread.sleep(5000);
+                }
 
-				log.info("SinaWeiboServiceImpl.write()" + weiboContent + "-" + (result != null ? result.getId() : ""));
-			}
-		}
+                log.info("SinaWeiboServiceImpl.write()" + weiboContent + "-" + (result != null ? result.getId() : ""));
+            }
+        }
 
-	}
+    }
 
 }
