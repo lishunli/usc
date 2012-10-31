138b4534918cc88d7a70e336b37e236e0117b79f
lishunli.me@gmail.com <lishunli.me@gmail.com@35b65ada-3664-11de-8fef-1f5ae417a2df>
2012-10-31 06:05:03 +0000

    nothing.

--- demo/src/main/java/org/usc/demo/synchronization/SynchronizationTest2.java
+++ demo/src/main/java/org/usc/demo/synchronization/SynchronizationTest2.java
@@ -9,57 +9,57 @@ import java.util.List;
  * @author ShunLi
  */
 public class SynchronizationTest2 {
-    public static void sync(String threadName, User user) {
-        String msg = threadName + ":" + user + ":";
-        System.out.println(msg + "sync1 start...");
+	public static void sync(String threadName, User user) {
+		String msg = threadName + ":" + user + ":";
+		System.out.println(msg + "sync1 start...");
 
-        synchronized (user) {
-            System.out.println(System.currentTimeMillis() + ":" + msg + "sync1 hit it");
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+		synchronized (user) {
+			System.out.println(System.currentTimeMillis() + ":" + msg + "sync1 hit it");
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 
-        System.out.println(msg + "sync1 end...");
-    }
+		System.out.println(msg + "sync1 end...");
+	}
 
-    public static void main(String[] args) {
-        User user1 = new User();
-        User user2 = new User();
-        User user3 = new User();
-        final List<User> users = Arrays.asList(user1, user2, user3);
+	public static void main(String[] args) {
+		User user1 = new User();
+		User user2 = new User();
+		User user3 = new User();
+		final List<User> users = Arrays.asList(user1, user2, user3);
 
-        for (int i = 0; i < 100; i++) {
-            new Thread(new Runnable() {
-                public void run() {
-                    String name = Thread.currentThread().getName();
-                    // System.out.println(name);
-                    sync(name, users.get((int) Thread.currentThread().getId() % 3));
-                }
-            }).start();
-        }
-    }
+		for (int i = 0; i < 100; i++) {
+			new Thread(new Runnable() {
+				public void run() {
+					String name = Thread.currentThread().getName();
+					// System.out.println(name);
+					sync(name, users.get((int) Thread.currentThread().getId() % 3));
+				}
+			}).start();
+		}
+	}
 
-    static class User {
-        private String userName;
-        private String passWord;
+	static class User {
+		private String userName;
+		private String passWord;
 
-        public String getUserName() {
-            return userName;
-        }
+		public String getUserName() {
+			return userName;
+		}
 
-        public void setUserName(String userName) {
-            this.userName = userName;
-        }
+		public void setUserName(String userName) {
+			this.userName = userName;
+		}
 
-        public String getPassWord() {
-            return passWord;
-        }
+		public String getPassWord() {
+			return passWord;
+		}
 
-        public void setPassWord(String passWord) {
-            this.passWord = passWord;
-        }
-    }
+		public void setPassWord(String passWord) {
+			this.passWord = passWord;
+		}
+	}
 }
