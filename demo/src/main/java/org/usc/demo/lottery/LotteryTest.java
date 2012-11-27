13c23ec7f3973375f0952101e94e353fd7e02c79
lishunli.me@gmail.com <lishunli.me@gmail.com@35b65ada-3664-11de-8fef-1f5ae417a2df>
2012-11-27 16:09:17 +0000

    emergency fix.

--- demo/src/main/java/org/usc/demo/lottery/LotteryTest.java
+++ demo/src/main/java/org/usc/demo/lottery/LotteryTest.java
@@ -2,7 +2,11 @@ package org.usc.demo.lottery;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 /**
  * 不同概率抽奖
@@ -18,42 +22,42 @@ public class LotteryTest {
 		gifts.add(new ThreeTuple<String, String, String>("1", "物品1", "301"));
 		gifts.add(new ThreeTuple<String, String, String>("2", "物品2", "302"));
 		gifts.add(new ThreeTuple<String, String, String>("3", "物品3", "303"));
-		gifts.add(new ThreeTuple<String, String, String>("4", "物品4", "304"));
-		gifts.add(new ThreeTuple<String, String, String>("5", "物品5", "305"));
+		// gifts.add(new ThreeTuple<String, String, String>("4", "物品4", "304"));
+		// gifts.add(new ThreeTuple<String, String, String>("5", "物品5", "305"));
 
 		// List<Double> orignalRates = null;// Arrays.asList(0.4d, 0.1d, 0.3d, 0.2d);new ArrayList<Double>();//
-		List<Double> orignalRates = Arrays.asList(0.4d, 0.1d, 0.3d, 0.2d, 0.123d);
+		// List<Double> orignalRates = Arrays.asList(0.4d, 0.1d, 0.3d, 0.2d, 0.123d);
+		List<Double> orignalRates = Arrays.asList(0.2d, 0.4d, 0.4d);
 
-		for (int i = 0; i < 10000; i++) {
-			try {
-				ThreeTuple<String, String, String> tuple = gifts.get(LotteryUtil.lottery(orignalRates));
-				System.out.println(tuple);
-			} catch (Exception e) {
-				System.out.println("config is error,please check it!");
-			}
+		// for (int i = 0; i < 10000; i++) {
+		// try {
+		// ThreeTuple<String, String, String> tuple = gifts.get(LotteryUtil.lottery(orignalRates));
+		// System.out.println(tuple);
+		// } catch (Exception e) {
+		// System.out.println("config is error,please check it!");
+		// }
+		// }
+
+		// calc
+		Map<ThreeTuple<String, String, String>, Integer> count = new HashMap<ThreeTuple<String, String, String>, Integer>();
+
+		for (int i = 0; i < 1000000; i++) {
+			int orignalIndex = LotteryUtil.lottery(orignalRates);
+
+			ThreeTuple<String, String, String> tuple = gifts.get(orignalIndex);
+			Integer value = count.get(tuple);
+			count.put(tuple, value == null ? 1 : value + 1);
 		}
 
-		// // calc
-		// Map<ThreeTuple<String, String, String>, Integer> count = new HashMap<ThreeTuple<String, String, String>, Integer>();
-		//
-		// for (int i = 0; i < 100000; i++) {
-		// int orignalIndex = lottery(orignalRates);
-		//
-		// ThreeTuple<String, String, String> tuple = gifts.get(orignalIndex);
-		// Integer value = count.get(tuple);
-		// count.put(tuple, value == null ? 1 : value + 1);
-		// }
-		//
-		// System.out.println(count);
-		//
-		// Set<Entry<ThreeTuple<String, String, String>, Integer>> entrySet = count.entrySet();
-		// double sum = 0;
-		// for (Entry<ThreeTuple<String, String, String>, Integer> entry : entrySet) {
-		// sum += entry.getValue();
-		// }
-		// for (Entry<ThreeTuple<String, String, String>, Integer> entry : entrySet) {
-		// System.out.println(entry.getKey() + ":" + entry.getValue() / sum);
-		// }
-	}
+		System.out.println(count);
 
+		Set<Entry<ThreeTuple<String, String, String>, Integer>> entrySet = count.entrySet();
+		double sum = 0;
+		for (Entry<ThreeTuple<String, String, String>, Integer> entry : entrySet) {
+			sum += entry.getValue();
+		}
+		for (Entry<ThreeTuple<String, String, String>, Integer> entry : entrySet) {
+			System.out.println(entry.getKey() + ":" + entry.getValue() / sum);
+		}
+	}
 }
--- demo/src/main/java/org/usc/demo/lottery/LotteryUtil.java
+++ demo/src/main/java/org/usc/demo/lottery/LotteryUtil.java
@@ -3,52 +3,41 @@ package org.usc.demo.lottery;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 
 /**
  *
  * @author Shunli
  */
 public class LotteryUtil {
-	private static final Random random = new Random();
-
 	public static int lottery(List<Double> orignalRates) {
 		if (orignalRates == null || orignalRates.isEmpty()) {
 			return -1;
 		}
 
 		int size = orignalRates.size();
-
 		List<Double> sortOrignalRates = new ArrayList<Double>(size);
-		sortOrignalRates.addAll(orignalRates);
-		Collections.sort(sortOrignalRates);
 
-		List<Double> rates = new ArrayList<Double>(size);
 		Double sumRate = 0d;
-		for (Double rate : sortOrignalRates) {
+		for (Double rate : orignalRates) {
 			sumRate += rate;
 		}
 
 		Double tempSumRate = 0d;
-		for (Double rate : sortOrignalRates) {
+		for (Double rate : orignalRates) {
 			tempSumRate += rate;
-			rates.add(tempSumRate / sumRate);
+			sortOrignalRates.add(tempSumRate / sumRate);
 		}
 
-		double result = rates.get(0);
-		double nextDouble = random.nextDouble();
+		double nextDouble = Math.random();
+		sortOrignalRates.add(nextDouble);
+		Collections.sort(sortOrignalRates);
 
-		for (double rate : rates) {
-			if (nextDouble >= rate) {
-				continue;
-			}
-			result = rate;
-			break;
+		int indexOf = sortOrignalRates.indexOf(nextDouble);
+		// safely return. very minor times nextDouble in the last of sortOrignalRates(after sort) list.
+		if (indexOf >= size) {
+			return 0; // In general 0 index the probability is the largest
+
 		}
-
-		int index = rates.indexOf(result);
-		int orignalIndex = orignalRates.indexOf(sortOrignalRates.get(index));
-
-		return orignalIndex;
+		return indexOf;
 	}
 }
