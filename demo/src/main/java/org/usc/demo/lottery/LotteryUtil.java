36463608c87beafde062522617fa3d92944d318a
lishunli.me@gmail.com <lishunli.me@gmail.com@35b65ada-3664-11de-8fef-1f5ae417a2df>
2012-11-28 02:10:44 +0000

    think more.

--- demo/src/main/java/org/usc/demo/lottery/LotteryUtil.java
+++ demo/src/main/java/org/usc/demo/lottery/LotteryUtil.java
@@ -9,35 +9,29 @@ import java.util.List;
  * @author Shunli
  */
 public class LotteryUtil {
-	public static int lottery(List<Double> orignalRates) {
-		if (orignalRates == null || orignalRates.isEmpty()) {
-			return -1;
-		}
+    public static int lottery(List<Double> orignalRates) {
+        if (orignalRates == null || orignalRates.isEmpty()) {
+            return -1;
+        }
 
-		int size = orignalRates.size();
-		List<Double> sortOrignalRates = new ArrayList<Double>(size);
+        int size = orignalRates.size();
+        List<Double> sortOrignalRates = new ArrayList<Double>(size);
 
-		Double sumRate = 0d;
-		for (Double rate : orignalRates) {
-			sumRate += rate;
-		}
+        Double sumRate = 0d;
+        for (Double rate : orignalRates) {
+            sumRate += rate;
+        }
 
-		Double tempSumRate = 0d;
-		for (Double rate : orignalRates) {
-			tempSumRate += rate;
-			sortOrignalRates.add(tempSumRate / sumRate);
-		}
+        Double tempSumRate = 0d;
+        for (Double rate : orignalRates) {
+            tempSumRate += rate;
+            sortOrignalRates.add(tempSumRate / sumRate);
+        }
 
-		double nextDouble = Math.random();
-		sortOrignalRates.add(nextDouble);
-		Collections.sort(sortOrignalRates);
+        double nextDouble = Math.random();
+        sortOrignalRates.add(nextDouble);
+        Collections.sort(sortOrignalRates);
 
-		int indexOf = sortOrignalRates.indexOf(nextDouble);
-		// safely return. very minor times nextDouble in the last of sortOrignalRates(after sort) list.
-		if (indexOf >= size) {
-			return 0; // In general 0 index the probability is the largest
-
-		}
-		return indexOf;
-	}
+        return sortOrignalRates.indexOf(nextDouble);
+    }
 }
