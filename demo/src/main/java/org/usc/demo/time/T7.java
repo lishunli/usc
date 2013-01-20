29292d6d8b9d5ed3b31f4f3ebd2e3cf100b3ba9b
lishunli.me@gmail.com <lishunli.me@gmail.com@35b65ada-3664-11de-8fef-1f5ae417a2df>
2013-01-20 08:38:44 +0000

    add get now date and check is holiday demo.

---
+++ demo/src/main/java/org/usc/demo/time/T7.java
@@ -0,0 +1,42 @@
+package org.usc.demo.time;
+
+import java.util.Calendar;
+
+import org.json.JSONObject;
+
+import com.xunlei.game.activity.web.url.HttpRequester;
+import com.xunlei.game.activity.web.url.HttpRespons;
+
+/**
+ *
+ * @author ShunLi
+ */
+public class T7 {
+    private static final HttpRequester httpReq = new HttpRequester("utf-8", 6000, 6000);
+    private static final Calendar cal = Calendar.getInstance();
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(isHoliday());
+    }
+
+    private static boolean isHoliday() {
+        try {
+            HttpRespons respon = httpReq.sendGet("http://act.game.xunlei.com:85/xlgame_youxi/servertime");
+            String jsonRtn = respon.getContent();
+
+            long now = new JSONObject(jsonRtn).getLong("result");
+            cal.setTimeInMillis(now);
+
+            int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+
+            if (dayOfWeek == 7 || dayOfWeek == 1) {
+                return true;
+            }
+
+        } catch (Exception e) {
+            // no-op, return defalult false
+        }
+
+        return false;
+    }
+}
