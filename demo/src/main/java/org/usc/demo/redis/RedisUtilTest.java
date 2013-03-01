0a554f7517320cab0a1f76cfe6d5d1548f3ccad7
lishunli.me@gmail.com <lishunli.me@gmail.com@35b65ada-3664-11de-8fef-1f5ae417a2df>
2013-03-01 09:04:11 +0000

    support more commands.

--- demo/src/main/java/org/usc/demo/redis/RedisUtilTest.java
+++ demo/src/main/java/org/usc/demo/redis/RedisUtilTest.java
@@ -21,7 +21,10 @@ public class RedisUtilTest {
     public static void main(String[] args) {
         // setGet();
         // setnx();
-        setexTtl();
+        // setexTtl();
+        // existsDel();
+        // incr();
+        // decr();
         // setGetObject1();
         // setGetObject2();
     }
@@ -68,6 +71,57 @@ public class RedisUtilTest {
 
         System.out.println(RedisUtil.ttl(key));
     }
+
+    public static void existsDel() {
+        System.out.println(StringUtils.center(" del ", 60, "="));
+
+        String key = KEY_PREFIX + RandomStringUtils.randomAlphabetic(6);
+        String value = "test";
+
+        System.out.println(key);
+        RedisUtil.set(key, value);
+        System.out.println(RedisUtil.get(key));
+
+        System.out.println(RedisUtil.exists(key));
+        System.out.println(RedisUtil.del(key));
+        System.out.println(RedisUtil.exists(key));
+        System.out.println(RedisUtil.del(key));
+    }
+
+    public static void incr() {
+        System.out.println(StringUtils.center(" incr ", 60, "="));
+
+        String key = KEY_PREFIX + RandomStringUtils.randomAlphabetic(6);
+
+        System.out.println(key);
+        System.out.println(RedisUtil.incr(key));
+        System.out.println(RedisUtil.incr(key));
+        System.out.println(RedisUtil.del(key));
+        System.out.println(RedisUtil.exists(key));
+        System.out.println(RedisUtil.incr(key));
+        System.out.println(RedisUtil.incrBy(key, 100));
+
+        RedisUtil.set(key, "test");
+        System.out.println(RedisUtil.incr(key));
+    }
+
+    public static void decr() {
+        System.out.println(StringUtils.center(" decr ", 60, "="));
+
+        String key = KEY_PREFIX + RandomStringUtils.randomAlphabetic(6);
+
+        System.out.println(key);
+        System.out.println(RedisUtil.decr(key));
+
+        RedisUtil.set(key, "100");
+        System.out.println(RedisUtil.decr(key));
+        System.out.println(RedisUtil.decr(key));
+        System.out.println(RedisUtil.decrBy(key, 50));
+
+        RedisUtil.set(key, "test");
+        System.out.println(RedisUtil.incr(key));
+    }
+
     public static void setGetObject1() {
         System.out.println(StringUtils.center(" setGetObject1 ", 60, "="));
 
